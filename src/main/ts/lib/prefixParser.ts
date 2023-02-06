338085be3f599d04ad8d2cfc786d1c494884621b
Snap <XxSnapperGeexX@gmail.com>
2023-02-05 16:57:21 -0800

    Remove nested if else blocks from CliArgPrefixParse class.

--- src/main/ts/lib/prefixParser.ts
+++ src/main/ts/lib/prefixParser.ts
@@ -1,5 +1,6 @@
 import {OperandsFlagsOptions} from "./operandsFlagsOptions.js";
 import {joinStringsFormatted} from "./util.js";
+
 export class CliArgPrefixParser extends OperandsFlagsOptions
 {
     private readonly _prefixChar: string;
@@ -9,32 +10,17 @@ export class CliArgPrefixParser extends OperandsFlagsOptions
 
     public constructor(prefixChar: string, strings: readonly string[])
     {
-        // If no strings to parse are passed...
-        if (strings.length === 0)
-        {
-            super([], [], []);
-            this._strings = Object.freeze([]);
-            this._distinct = new OperandsFlagsOptions( Object.freeze([]),
-                                                       Object.freeze([]),
-                                                       Object.freeze([]) );
-        }
-        // Else if prefix char is blank...
-        else if (prefixChar.length === 0)
-        {
-            super(strings, [], []);
-            this._strings = this._all;
-            this._distinct = new OperandsFlagsOptions( Object.freeze([...new Set(this._all)]),
-                                                       Object.freeze([]),
-                                                       Object.freeze([]) );
-        }
-        else
-        {
             const optionPrefix: string = prefixChar.repeat(2);
 
-            const operandsFlagsOptions: { readonly operands: string[],
-                                          readonly flags: string[],
-                                          readonly options: string[] }
-                = strings.reduce((operandFlagOptionsTuple: {readonly operands: string[], readonly options: string[], readonly flags: string[]}, aString: string) =>
+            const operandsFlagsOptions: Readonly<{ readonly operands: string[],
+                                                   readonly flags: string[],
+                                                   readonly options: string[] }>
+                = Object.freeze(strings
+                    .reduce((operandFlagOptionsTuple: Readonly<{
+                        readonly operands: string[],
+                        readonly options: string[],
+                        readonly flags: string[] }>,
+                             aString: string) =>
                 {
                     // If no prefix char, save entire string to index
                     if ( ! aString.startsWith(prefixChar))
@@ -58,23 +44,18 @@ export class CliArgPrefixParser extends OperandsFlagsOptions
                     }
                     return operandFlagOptionsTuple;
                 },
-                Object.freeze({operands: [], flags: [],options: []}));
+                Object.freeze({operands: [], flags: [],options: []})));
 
             super( Object.freeze(operandsFlagsOptions.operands),
                    Object.freeze(operandsFlagsOptions.flags),
                    Object.freeze(operandsFlagsOptions.options) );
-
+            this._prefixChar = prefixChar;
             this._strings = strings;
-
             this._distinct = Object.freeze(new OperandsFlagsOptions(
                 Object.freeze([...new Set(this._operands)]),
                 Object.freeze([...new Set(this._flags)]),
                 Object.freeze([...new Set(this._options)])
             ));
-        }
-
-        this._prefixChar = prefixChar;
-
     }
 
     public prefixChar(): string {return this._prefixChar;}
@@ -91,12 +72,25 @@ export class CliArgPrefixParser extends OperandsFlagsOptions
 
     public distinct(): Readonly<OperandsFlagsOptions> {return this._distinct;}
 
-    public toString(): string
+    public toString(format: string | undefined | null = undefined): string
     {
-        const str: string =   `prefixChar: "${this._prefixChar}", `
-                            + `operands: ${joinStringsFormatted(this._operands)}, `
-                            + `flags: ${joinStringsFormatted(this._flags)}, `
-                            + `options: ${joinStringsFormatted(this._options)}`;
+        const str: string =    format === undefined
+                            || format === null
+                            || format.length === 0
+                            || ["inline", "none"].some(frmt => strEqualsIgnoreCase(format, frmt))
+            ?   `prefixChar: "${this._prefixChar}", `
+              + `operands: ${joinStringsFormatted(this._operands)}, `
+              + `flags: ${joinStringsFormatted(this._flags)}, `
+              + `options: ${joinStringsFormatted(this._options)}`
+            : "\n" === format
+                ?   `prefixChar: "${this._prefixChar}", `
+                  + `operands: ${joinStringsFormatted(this._operands)}, `
+                  + `flags: ${joinStringsFormatted(this._flags)}, `
+                  + `options: ${joinStringsFormatted(this._options)}`
+            : "\n";
+
         return `${this.constructor.name}{${str}}`;
     }
 }
+
+const strEqualsIgnoreCase = (aString: string, anotherString: string): boolean =>  aString.localeCompare(anotherString, undefined, {sensitivity: "base"}) === 0;
