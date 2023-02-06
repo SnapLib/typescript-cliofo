b7f5cfea04c932948f3b815885f28bb6de904aea
Snap <XxSnapperGeexX@gmail.com>
2023-02-05 23:05:53 -0800

    Add doc comment to util.joinStringsFormatted(readonly string[]) function.

--- src/main/ts/lib/util.ts
+++ src/main/ts/lib/util.ts
@@ -1,3 +1,9 @@
+/**
+ * Joins together an array of strings in a pretty way.
+ *
+ * @param strings The strings to join together.
+ * @returns the provided strings joined together as a single string.
+ */
 export const joinStringsFormatted = (strings: readonly string[]): string =>
 {
     const quotes: string = strings.length !== 0 ? "\"" : "";
