972c2ca68a140b30eed76f4f1ced1211367267c0
Snap <XxSnapperGeexX@gmail.com>
2023-02-05 19:11:26 -0800

    Make prefixParser generate new immutable objects in constructor.

--- src/main/ts/lib/prefixParser.ts
+++ src/main/ts/lib/prefixParser.ts
@@ -12,38 +12,47 @@ export class CliArgPrefixParser extends OperandsFlagsOptions
     {
             const optionPrefix: string = prefixChar.repeat(2);
 
-            const operandsFlagsOptions: Readonly<{ readonly operands: string[],
-                                                   readonly flags: string[],
-                                                   readonly options: string[] }>
+            const operandsFlagsOptions: Readonly<{ readonly operands: readonly string[],
+                                                   readonly flags: readonly string[],
+                                                   readonly options: readonly string[] }>
                 = strings.reduce((
-                    operandFlagOptionsTuple: Readonly<{ readonly operands: string[],
-                                                        readonly options: string[],
-                                                        readonly flags: string[] }>,
+                    operandFlagOptionsTuple: Readonly<{ readonly operands: readonly string[],
+                                                        readonly options: readonly string[],
+                                                        readonly flags: readonly string[] }>,
                     aString: string) =>
                 {
                     // If no prefix char, save entire string to index
                     if ( ! aString.startsWith(prefixChar))
                     {
-                        operandFlagOptionsTuple.operands.push(aString);
+                        return Object.freeze({
+                                   operands: Object.freeze([...operandFlagOptionsTuple.operands, aString]),
+                                   flags: (operandFlagOptionsTuple.flags),
+                                   options: operandFlagOptionsTuple.options });
                     }
                     // If starts with 2 or more adjacent prefix chars, save string
                     // without leading 2 prefix chars
                     else if (aString.startsWith(optionPrefix))
                     {
-                        operandFlagOptionsTuple.options.push(aString.slice(optionPrefix.length));
+                        return Object.freeze({
+                                   operands: operandFlagOptionsTuple.operands,
+                                   flags: (operandFlagOptionsTuple.flags),
+                                   options: Object.freeze([...operandFlagOptionsTuple.options, aString.slice(optionPrefix.length)]) });
                     }
                     // If starts with only a single prefix char, save characters of
                     // string without leading prefix char
                     else
                     {
-                        for(const flagChar of aString.slice(prefixChar.length))
-                        {
-                            operandFlagOptionsTuple.flags.push(flagChar);
-                        }
+                        return Object.freeze({
+                                   operands: operandFlagOptionsTuple.operands,
+                                   flags: Object.freeze([...operandFlagOptionsTuple.flags, ...aString.slice(prefixChar.length)]),
+                                   options: operandFlagOptionsTuple.options});
                     }
-                    return operandFlagOptionsTuple;
                 },
-                Object.freeze({operands: [], flags: [],options: []}));
+                // Initial frozen empty operands, flags, and options object
+                Object.freeze({ operands: Object.freeze([]),
+                                flags: Object.freeze([]),
+                                options: Object.freeze([]) }
+                ));
 
             super( Object.freeze(operandsFlagsOptions.operands),
                    Object.freeze(operandsFlagsOptions.flags),
@@ -71,10 +80,10 @@ export class CliArgPrefixParser extends OperandsFlagsOptions
 
     public distinct(): Readonly<OperandsFlagsOptions> {return this._distinct;}
 
-    public toString(format: string | undefined | null = undefined, tabSize: number = 2): string
+    public toString(format: string | undefined | null = null, tabSize: number | undefined | null = null): string
     {
         const validFormatArgs: readonly (string | undefined | null)[] =
-            Object.freeze([undefined, null, "inline", "none", "\n", "\n\n", "\u0020"]);
+            Object.freeze([undefined, null, "inline", "none", "verbose", "\n", "\n\n", "\u0020"]);
 
         if ( ! validFormatArgs.some(validFrmtArg => strEqualsIgnoreCase(validFrmtArg, format)))
         {
@@ -83,25 +92,29 @@ export class CliArgPrefixParser extends OperandsFlagsOptions
             throw new Error(`illegal string format argument: ${quotes}${format}${quotes}`);
         }
 
-        return "\u0020" === format
-                   ?   `\u0020prefixChar: "${this._prefixChar}", `
-                     + `operands: ${joinStringsFormatted(this._operands)}, `
-                     + `flags: ${joinStringsFormatted(this._flags)}, `
-                     + `options: ${joinStringsFormatted(this._options)} `
-              : "\n" === format
-                  ?   `${" ".repeat(tabSize)}prefixChar: "${this._prefixChar}",\n`
-                    + `${" ".repeat(tabSize)}operands: ${joinStringsFormatted(this._operands)},\n`
-                    + `${" ".repeat(tabSize)}flags: ${joinStringsFormatted(this._flags)},\n`
-                    + `${" ".repeat(tabSize)}options: ${joinStringsFormatted(this._options)}\n`
-              : "\n\n" === format
-                  ?   `\n${" ".repeat(tabSize)}prefixChar: "${this._prefixChar}",\n`
-                    + `${" ".repeat(tabSize)}operands: ${joinStringsFormatted(this._operands)},\n`
-                    + `${" ".repeat(tabSize)}flags: ${joinStringsFormatted(this._flags)},\n`
-                    + `${" ".repeat(tabSize)}options: ${joinStringsFormatted(this._options)}\n`
-              :   `prefixChar: "${this._prefixChar}", `
-                + `operands: ${joinStringsFormatted(this._operands)}, `
-                + `flags: ${joinStringsFormatted(this._flags)}, `
-                + `options: ${joinStringsFormatted(this._options)}`;
+        const _tabSize: number = tabSize === undefined ? 0 : tabSize === null ? 4 : tabSize;
+
+        return   `${OperandsFlagsOptions.name}{`
+               + ( "\u0020" === format
+                     ?   `\u0020prefixChar: "${this._prefixChar}", `
+                       + `operands: ${joinStringsFormatted(this._operands)}, `
+                       + `flags: ${joinStringsFormatted(this._flags)}, `
+                       + `options: ${joinStringsFormatted(this._options)}\u0020`
+                   : "\n" === format
+                       ?   `prefixChar: "${this._prefixChar}",\n`
+                         + `${" ".repeat(_tabSize)}operands: ${joinStringsFormatted(this._operands)},\n`
+                         + `${" ".repeat(_tabSize)}flags: ${joinStringsFormatted(this._flags)},\n`
+                         + `${" ".repeat(_tabSize)}options: ${joinStringsFormatted(this._options)}\n`
+                   : "\n\n" === format
+                       ?   `\n${" ".repeat(_tabSize)}prefixChar: "${this._prefixChar}",\n`
+                         + `${" ".repeat(_tabSize)}operands: ${joinStringsFormatted(this._operands)},\n`
+                         + `${" ".repeat(_tabSize)}flags: ${joinStringsFormatted(this._flags)},\n`
+                         + `${" ".repeat(_tabSize)}options: ${joinStringsFormatted(this._options)}\n`
+                   :    `prefixChar: "${this._prefixChar}", `
+                      + `operands: ${joinStringsFormatted(this._operands)}, `
+                      + `flags: ${joinStringsFormatted(this._flags)}, `
+                      + `options: ${joinStringsFormatted(this._options)}`)
+               + "}";
     }
 }
 
