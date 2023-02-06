7fb65f5e8618e56f2e32766370e0e5495379b942
Snap <XxSnapperGeexX@gmail.com>
2023-02-05 22:10:27 -0800

    Add doc comments to OperandsFlagsOptions class.

--- src/main/ts/lib/operandsFlagsOptions.ts
+++ src/main/ts/lib/operandsFlagsOptions.ts
@@ -1,3 +1,7 @@
+/**
+ * A container for string arrays intended to be used for strings parsed to
+ * operands, flags, and flags.
+ */
 export class OperandsFlagsOptions
 {
     protected readonly _all: readonly string[];
@@ -11,13 +15,38 @@ export class OperandsFlagsOptions
     protected constructor(operands: readonly string[], flags: readonly string[], options: readonly string[])
     {
         this._all = Object.freeze([...operands, ...flags, ...options]);
-        this._operands = operands;
-        this._flags = flags;
-        this._options = options;
+        this._operands = Object.isFrozen(operands) ? operands : Object.freeze([...operands]);
+        this._flags = Object.isFrozen(flags) ? flags : Object.freeze([...flags]);
+        this._options = Object.isFrozen(options) ? options : Object.freeze([...options]);
     }
 
+    /**
+     * Returns all of this objects operand, flag, and option strings combined
+     * into a single array.
+     *
+     * @returns all of this objects operand, flag, and option strings combined
+     *          into a single array.
+     */
     public all(): readonly string[] {return this._all;}
+
+    /**
+     * Returns this object's operand strings as an array.
+     *
+     * @returns this object's operand strings as an array.
+     */
     public operands(): readonly string[] {return this._operands;}
+
+    /**
+     * Returns this object's flag strings as an array.
+     *
+     * @returns this object's flag strings as an array.
+     */
     public flags(): readonly string[] {return this._flags;}
+
+    /**
+     * Returns this object's option strings as an array.
+     *
+     * @returns this object's option strings as an array.
+     */
     public options(): readonly string[] {return this._options;}
 }
