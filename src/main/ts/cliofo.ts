ce381b6e1bec7f6632603cb8733d293fce37cfcd
Snap <XxSnapperGeexX@gmail.com>
2023-02-09 22:37:09 -0800

    Remove debug log statements and commented out code from cliofo.ts file.

--- src/main/ts/cliofo.ts
+++ src/main/ts/cliofo.ts
@@ -3,40 +3,30 @@ import PrefixParser from "./lib/prefixParser.js";
 
 export class Cliofo
 {
-    readonly #parsedPrefix: Readonly<PrefixParser>;
-    readonly #occurrenceCount: Readonly<CliofoPrefixOccurrenceCount>;
+    public readonly parsedPrefix: Readonly<PrefixParser>;
+    public readonly occurrenceCount: Readonly<CliofoPrefixOccurrenceCount>;
 
     public constructor(prefixString: string, strings: readonly string[])
     {
-        this.#parsedPrefix = Object.freeze(new PrefixParser(prefixString, strings));
+        this.parsedPrefix = Object.freeze(new PrefixParser(prefixString, strings));
 
-        this.#occurrenceCount = Object.freeze(new CliofoPrefixOccurrenceCount(this.#parsedPrefix));
+        this.occurrenceCount = Object.freeze(new CliofoPrefixOccurrenceCount(this.parsedPrefix));
     }
-
-    public prefixParsed(): Readonly<PrefixParser> {return this.#parsedPrefix;}
-    public occurrenceCount(): Readonly<CliofoPrefixOccurrenceCount> {return this.#occurrenceCount;}
 
     public toJSON(format: Partial<{verbose: boolean, replacer: (_: unknown) => unknown, space: string | number}> = {}): string
     {
         return format.verbose
                    ? JSON.stringify({
-                        prefixString: this.#parsedPrefix.prefixString,
-                        strings: this.#parsedPrefix.strings,
-                        operands: this.#parsedPrefix.operands,
-                        flags: this.#parsedPrefix.flags,
-                        options: this.#parsedPrefix.options
+                        prefixString: this.parsedPrefix.prefixString,
+                        strings: this.parsedPrefix.strings,
+                        operands: this.parsedPrefix.operands,
+                        flags: this.parsedPrefix.flags,
+                        options: this.parsedPrefix.options
                     },
                     format.replacer,
                     format.space)
-                   : this.#occurrenceCount.toJSON({replacer: format.replacer,
-                                                   space: format.space});
+                   : JSON.stringify(this);
     }
 }
 
 export {Cliofo as default};
-
-// console.log(new Cliofo("-", process.argv.slice(2)).prefixParsed().distinct());
-// console.log(new Cliofo("-", process.argv.slice(2)).occurrenceCount().toJSON({space: 2}));
-// console.log(new Cliofo("-", process.argv.slice(2)).occurrenceCount().flagsOccurrenceCountMap());
-// console.log(new Cliofo("-", process.argv.slice(2)).toJSON({verbose: true, space: 2}));
-console.log(new Cliofo("-", process.argv.slice(2)).prefixParsed());
