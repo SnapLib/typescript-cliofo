3edd1f31275d8b7b9c7df4859dfe6dd2eaf58c1c
Snap <XxSnapperGeexX@gmail.com>
2023-02-09 23:43:48 -0800

    Add distinct property to Cliofo class.

--- src/main/ts/cliofo.ts
+++ src/main/ts/cliofo.ts
@@ -1,16 +1,25 @@
-import CliofoPrefixOccurrenceCount from "./lib/cliofoPrefixOccurrenceCount.js";
-import PrefixParser from "./lib/cliofoPrefixParse.js";
+import {OperandsFlagsOptions} from "lib/operandsFlagsOptions.js";
+import {CliofoPrefixOccurrenceCount} from "./lib/cliofoPrefixOccurrenceCount.js";
+import {CliofoPrefixParse} from "./lib/cliofoPrefixParse.js";
 
 export class Cliofo
 {
-    public readonly parsedPrefix: Readonly<PrefixParser>;
+    public readonly parsedPrefix: Readonly<CliofoPrefixParse>;
+
     public readonly occurrenceCount: Readonly<CliofoPrefixOccurrenceCount>;
+
+    public readonly distinct: Readonly<OperandsFlagsOptions>;
 
     public constructor(prefixString: string, strings: readonly string[])
     {
-        this.parsedPrefix = Object.freeze(new PrefixParser(prefixString, strings));
-
+        this.parsedPrefix = Object.freeze(new CliofoPrefixParse(prefixString, strings));
         this.occurrenceCount = Object.freeze(new CliofoPrefixOccurrenceCount(this.parsedPrefix));
+        this.distinct = Object.freeze({
+            operands: Object.freeze([...new Set(this.parsedPrefix.operands)]),
+            flags: Object.freeze([...new Set(this.parsedPrefix.flags)]),
+            options: Object.freeze([...new Set(this.parsedPrefix.options)]),
+            all: Object.freeze([...new Set(this.parsedPrefix.all)])
+        });
     }
 
     public toJSON(format: Partial<{verbose: boolean, replacer: (_: unknown) => unknown, space: string | number}> = {}): string
@@ -30,3 +39,9 @@ export class Cliofo
 }
 
 export {Cliofo as default};
+
+// console.log(new Cliofo("-", process.argv.slice(2)).prefixParsed().distinct());
+// console.log(new Cliofo("-", process.argv.slice(2)).occurrenceCount().toJSON({space: 2}));
+// console.log(new Cliofo("-", process.argv.slice(2)).occurrenceCount().flagsOccurrenceCountMap());
+// console.log(new Cliofo("-", process.argv.slice(2)).toJSON({verbose: true, space: 2}));
+console.log(new Cliofo("-", process.argv.slice(2)).occurrenceCount);
