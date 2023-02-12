755d9144963436007af2682d2a6732f911eb6156
Snap <XxSnapperGeexX@gmail.com>
2023-02-11 19:15:06 -0800

    Rename CliofoPrefixCount public properties.

--- src/main/ts/lib/cliofoPrefixCount.ts
+++ src/main/ts/lib/cliofoPrefixCount.ts
@@ -9,38 +9,38 @@ export class CliofoPrefixCount
      * times each one occurs as its value.
      * @readonly
      */
-    public readonly operandsCount: ReadonlyMap<string, number>;
+    public readonly operandCounts: ReadonlyMap<string, number>;
 
     /**
      * A map containing the flag strings as keys paired up with the number of
      * times each one occurs as its value.
      * @readonly
      */
-    public readonly flagsCount: ReadonlyMap<string, number>;
+    public readonly flagCounts: ReadonlyMap<string, number>;
 
     /**
      * A map containing the option strings as keys paired up with the number of
      * times each one occurs as its value.
      * @readonly
      */
-    public readonly optionsCount: ReadonlyMap<string, number>;
+    public readonly optionCounts: ReadonlyMap<string, number>;
 
-    public readonly allCount: ReadonlyMap<string, number>;
+    public readonly allCounts: ReadonlyMap<string, number>;
 
     public constructor(operandFlagOptionStrings: Readonly<CliofoPrefixStrings>)
     {
         this.prefixParser = freezeOperandsFlagsOptionStrings(operandFlagOptionStrings);
 
-        this.operandsCount = Object.freeze(new Map([...new Set(this.prefixParser.operands)].map(operandString => Object.freeze(
+        this.operandCounts = Object.freeze(new Map([...new Set(this.prefixParser.operands)].map(operandString => Object.freeze(
             [operandString, operandFlagOptionStrings.operands.filter(otherOperandString => operandString === otherOperandString).length]) )));
 
-        this.flagsCount = Object.freeze(new Map([...new Set(this.prefixParser.flags)].map(flagString => Object.freeze(
+        this.flagCounts = Object.freeze(new Map([...new Set(this.prefixParser.flags)].map(flagString => Object.freeze(
             [flagString, operandFlagOptionStrings.flags.filter(otherFlagString => flagString === otherFlagString).length]) )));
 
-        this.optionsCount = Object.freeze(new Map([...new Set(this.prefixParser.options)].map(optionString => Object.freeze(
+        this.optionCounts = Object.freeze(new Map([...new Set(this.prefixParser.options)].map(optionString => Object.freeze(
             [optionString, operandFlagOptionStrings.options.filter(otherOptionString => optionString === otherOptionString).length]) )));
 
-        this.allCount = Object.freeze(new Map([...new Set(this.prefixParser.all)].map(operandFlagOption => Object.freeze(
+        this.allCounts = Object.freeze(new Map([...new Set(this.prefixParser.all)].map(operandFlagOption => Object.freeze(
             [ operandFlagOption,
               this.prefixParser.all.filter(otherOperandFlagOption => operandFlagOption === otherOperandFlagOption).length]) )));
     }
