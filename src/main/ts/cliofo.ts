225bf6b4a193e57d70522c7965562c41ee786d78
Snap <XxSnapperGeexX@gmail.com>
2023-02-13 12:21:46 -0800

    Add doc comments to Cliofo class.

--- src/main/ts/cliofo.ts
+++ src/main/ts/cliofo.ts
@@ -1,8 +1,27 @@
 import {CliofoCounts} from "./lib/cliofoCounts.js";
 import {CliofoStrings} from "./lib/cliofoStrings.js";
 
+/**
+ * The root class of the entire Cliofo package. Classes from this package's
+ * library are used to parse strings into operands, flags, and options
+ * based on a prefix string.
+ *
+ * @remarks This class doesn't directly parse/consume any string arguments.
+ * Functions that parse/consume command line arguments and create
+ * `Cliofo` objects are provided by this package instead such as {@link cliofo}.
+ */
 export class Cliofo
 {
+    /**
+     * The `string` to denote flags and options( and operands).
+     * @readonly
+     */
+    public readonly prefixString: string;
+
+    /**
+     * The `string[]` of arguments to parse with the {@link prefixString} `string`.
+     * @readonly
+     */
     public readonly cliofoStrings: Readonly<CliofoStrings>;
 
     public readonly occurrenceCount: Readonly<CliofoCounts>;
@@ -11,6 +30,12 @@ export class Cliofo
     public readonly operand: Readonly<{ strings: readonly string[],
                                         counts: ReadonlyMap<string, number> }>;
 
+    /**
+     * The flag strings contained withn this object's {@link cliofoStrings}
+     * string arguments when paresed with. Flags are stored as individual characters as opposed to
+     * entire strings (like {@link operand} and {@link option} strings).
+     * @readonly
+     */
     public readonly flag: Readonly<{ strings: readonly string[],
                                      counts: ReadonlyMap<string, number> }>;
 
@@ -23,6 +48,7 @@ export class Cliofo
     public constructor(cliofoStrings: Readonly<CliofoStrings>)
     {
         this.cliofoStrings = cliofoStrings;
+        this.prefixString = this.cliofoStrings.prefixString;
         this.occurrenceCount = Object.freeze(new CliofoCounts(this.cliofoStrings.prefixString, this.cliofoStrings.arguments));
 
         this.operand = Object.freeze({
@@ -68,6 +94,21 @@ export class Cliofo
     }
 }
 
+/**
+ * The passed `prefixString` string argument is used to parse the strings of the
+ * passed `argumentStrings` string array.
+ *
+ * @remarks Returns a new {@link Cliofo} object constructed with the passed sarguments.
+ *
+ * @param prefixString The leading prefix `string` used to denote which
+ *                     arguments are flags and options.
+ *
+ * @param argumentStrings The strings to parse with the prefix string (provided
+ *                        via the passed `prefixString` string argument).
+ *
+ * @returns a new {@link Cliofo} instance constructed with the provided
+ *          `prefixString` and `argumentStrings` argument.
+ */
 export const cliofo = (prefixString: string, argumentStrings: readonly string[]): Readonly<Cliofo> =>
     Object.freeze(new Cliofo(new CliofoStrings(prefixString, argumentStrings)));
 
