a153b3d1145536ebd35063a51c28d7b4fa5fa965
Snap <XxSnapperGeexX@gmail.com>
2023-02-15 18:37:02 -0800

    Add indexes:ReadonlyMap<string, readonly numer[]> property to ParsedCliofoArgument interface.

--- src/main/ts/lib/parsedCliofoArgument.ts
+++ src/main/ts/lib/parsedCliofoArgument.ts
@@ -2,6 +2,7 @@ export interface ParsedCliofoArgument
 {
     readonly strings: readonly string[]
     readonly counts: ReadonlyMap<string, number>
+    readonly indexes: ReadonlyMap<string, readonly number[]>
 }
 
 export {ParsedCliofoArgument as default};
