6d7cc28ac3a1832b96b4169e5329bc272f619f33
Snap <XxSnapperGeexX@gmail.com>
2023-03-06 08:10:42 -0800

    Improve CliofoCounts ctor string parsing.

--- src/main/ts/lib/strings/parser/cliofo-counts.ts
+++ src/main/ts/lib/strings/parser/cliofo-counts.ts
@@ -1,4 +1,5 @@
 import {CliofoPrefixParser} from "./cliofo-prefix-parser.js";
+import {StringParseError} from "./string-parse-error.js";
 
 export class CliofoCounts extends CliofoPrefixParser<ReadonlyMap<string, number>>
 {
@@ -44,12 +45,55 @@ export class CliofoCounts extends CliofoPrefixParser<ReadonlyMap<string, number>
     {
         super(prefixString, strings);
 
-        const distinctArgStrings: ReadonlyArray<string> = Object.freeze([...new Set(this.arguments)]);
+        const _distinctString: Readonly<{ operands: readonly string[],
+                                          flagChars: readonly string[],
+                                          options: readonly string[] }>
+            = Object.freeze(
+                [...new Set(this.arguments)]
+                .reduce(
+                    (ofo, aString) => {
+                        // if string is operand
+                        if (this.prefixString.length === 0 || ! aString.startsWith(this.prefixString))
+                        {
+                            return Object.freeze(
+                                { operands: Object.freeze([...ofo.operands, aString]),
+                                  flagChars: ofo.flagChars,
+                                  options: ofo.options });
+                        }
+                        // if string starts with prefix string
+                        else if (aString.startsWith(this.prefixString))
+                        {
+                            // if string is options
+                            if (aString.startsWith(this.optionPrefixString()))
+                            {
+                                return Object.freeze(
+                                    { operands: ofo.operands,
+                                      flagChars: ofo.flagChars,
+                                      options: Object.freeze([...ofo.options, aString]) });
+                            }
+                            // if string is flag
+                            else
+                            {
+                                return Object.freeze(
+                                    { operands: ofo.operands,
+                                      flagChars: Object.freeze([...new Set([...ofo.flagChars, ...aString.slice(this.prefixString.length)])]),
+                                      options: ofo.options });
+                            }
+                        }
+                        else
+                        {
+                            throw new StringParseError(`could not parse string to operand, flag, or options: "${aString}"`, aString);
+                        }
+                    },
+                    { operands: Object.freeze(new Array<string>()),
+                      flagChars: Object.freeze(new Array<string>()),
+                      options: Object.freeze(new Array<string>()) }
+                )
+            );
 
         this.operand = Object.freeze(
             new Map(
-                distinctArgStrings
-                    .filter(aString => this.prefixString.length === 0 || ! aString.startsWith(this.prefixString))
+                _distinctString.operands
                     .map(aString =>
                         Object.freeze([ aString,
                           this.arguments.filter(anotherString => aString === anotherString).length ]))
@@ -58,17 +102,7 @@ export class CliofoCounts extends CliofoPrefixParser<ReadonlyMap<string, number>
 
         this.flag = Object.freeze(
             new Map(
-                distinctArgStrings
-                    .filter(aString =>    this.prefixString.length !== 0
-                                       && aString.startsWith(this.prefixString)
-                                       && ! aString.startsWith(this.optionPrefixString()))
-                    // Get every character of the string after the leading prefix string
-                    .flatMap(aString => [...new Set(aString.slice(this.prefixString.length))])
-                    // Collect every string character to a distinct array of characters
-                    .reduce(
-                        (charArray, aChar) =>
-                            { return [...new Set([...charArray, aChar])];},
-                        new Array<string>())
+                _distinctString.flagChars
                     // Create array of distinct character keys paired with the
                     // number of times they occur in strings
                     .map(aChar =>
@@ -91,8 +125,7 @@ export class CliofoCounts extends CliofoPrefixParser<ReadonlyMap<string, number>
 
         this.option = Object.freeze(
             new Map(
-                distinctArgStrings
-                    .filter(aString => this.prefixString.length !== 0 && aString.startsWith(this.optionPrefixString()))
+                _distinctString.options
                     .map(aString =>
                         Object.freeze([
                             aString.slice(this.optionPrefixString().length),
