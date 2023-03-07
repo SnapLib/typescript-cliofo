60db84277e41254c57b7a7b26fe7aabd3b2ec0ef
Snap <XxSnapperGeexX@gmail.com>
2023-03-06 17:15:36 -0800

    Add doc comments and getter for static TypeConsumer.#defaultConvertedStringPredicate to TypeConsumer class.

--- src/main/ts/lib/strings/consumer/type-consumer.ts
+++ src/main/ts/lib/strings/consumer/type-consumer.ts
@@ -1,16 +1,62 @@
 import {StringConsumer} from "./string-consumer";
 
-export class TypeConsumer<T> extends StringConsumer
+export class TypeConsumer<ConvertedStringType> extends StringConsumer
 {
-    readonly #stringConverter: (aString: string) => T;
+    public readonly stringConverter: (aString: string) => ConvertedStringType;
+
+    public readonly convertedStringPredicate: (convertedString: ConvertedStringType) => boolean;
+
+    static readonly #defaultConvertedStringPredicate = () => false;
 
     public constructor( stringValue: string,
                         range: Readonly<{min: number, max: number}>,
-                        stringConverter: (aString: string) => T )
+                        stringConverter: (aString: string) => ConvertedStringType,
+                        convertedStringPredicate: (convertedString: ConvertedStringType) => boolean = TypeConsumer.#defaultConvertedStringPredicate )
     {
         super(stringValue, range);
-        this.#stringConverter = stringConverter;
+        this.stringConverter = stringConverter;
+        this.convertedStringPredicate = convertedStringPredicate;
     }
+
+    /**
+     * Passes the provided `string` to this object's {@link stringConverter} and
+     * returns the resulting {@link ConvertedStringType} instance.
+     *
+     * @param aString The `string` to be converted to the {@link ConvertedStringType}
+     *        instance via this object's {@link stringConverter}.
+     *
+     * @returns An instance of a {@link ConvertedStringType} returned after
+     *          passing the provided `string` to this object's
+     *          {@link stringConverter}.
+     */
+    public convertString(aString: string): ConvertedStringType
+        { return this.stringConverter(aString); }
+
+    public convertedStringIsValid(convertedString: ConvertedStringType)
+    {
+        return this.convertedStringPredicate(convertedString);
+    }
+
+    /**
+     * Returns `true` if this object's converted string predicate is not the
+     * default converted string predicate.
+     *
+     * @returns `true` if this object's converted string predicate is not the
+     *          default converted string predicate.
+     */
+    public hasConvertedStringPredicate(): boolean
+        { return this.convertedStringPredicate !== TypeConsumer.#defaultConvertedStringPredicate; }
+
+    /**
+     * Returns the static default {@link ConvertedStringType} predicate.
+     *
+     * @returns The static default {@link ConvertedStringType} predicate.
+     *
+     * @protected
+     * @static
+     */
+    protected static defaultConvertedStringPredicate(): () => boolean
+        { return TypeConsumer.#defaultConvertedStringPredicate; }
 }
 
 export {TypeConsumer as default};
