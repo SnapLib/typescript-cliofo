2012cbcee7cf3f4aadbc4b9a908a330f2085a135
Snap <XxSnapperGeexX@gmail.com>
2023-03-12 17:57:52 -0700

    Use explicit type import of CliofoType and export OperandFlagOption as default.

--- src/main/ts/lib/operand-flag-option.ts
+++ src/main/ts/lib/operand-flag-option.ts
@@ -1,6 +1,6 @@
-import {CliofoType} from "./cliofo-type.js";
+import {type CliofoType} from "./cliofo-type.js";
 
 export type OperandFlagOption<T> =
     {readonly [OfoTypeString in keyof typeof CliofoType as Lowercase<OfoTypeString>]: T};
 
-export {OperandFlagOption as default};
+export {type OperandFlagOption as default};
