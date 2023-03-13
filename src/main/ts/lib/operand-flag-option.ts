2ad35ee3a78888a9e2ae80bea942e1aec53ab824
Snap <XxSnapperGeexX@gmail.com>
2023-03-12 19:09:42 -0700

    Add doc comments to OperandFlagOption type.

--- src/main/ts/lib/operand-flag-option.ts
+++ src/main/ts/lib/operand-flag-option.ts
@@ -1,5 +1,12 @@
 import {type CliofoType} from "./cliofo-type.js";
 
+/**
+ * A type referencing properties derived from the {@link CliofoType} enum mapped
+ * to values of the type specified by the passed type parameter.
+ *
+ * @param T The type of value each operand, flag, and option property are mapped
+ *          to.
+ */
 export type OperandFlagOption<T> =
     {readonly [OfoTypeString in keyof typeof CliofoType as Lowercase<OfoTypeString>]: T};
 
