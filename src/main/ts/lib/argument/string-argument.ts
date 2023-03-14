0952d4726d0fbad8fc9b67e1700f2f674682961d
Snap <XxSnapperGeexX@gmail.com>
2023-03-13 19:16:38 -0700

    Add doc comments to StringArgument class.

--- src/main/ts/lib/argument/string-argument.ts
+++ src/main/ts/lib/argument/string-argument.ts
@@ -15,18 +15,40 @@ export class StringArgument
     public readonly prefixString: string;
 
     /**
+     * Returns this argument's `string` value without the leading
+     * {@link prefixString} prepended to it.
+     *
      * @public
      * @readonly
      */
-    public readonly notPrefixed: string;
+    public readonly nonPrefixedString: string;
 
     /**
+     * Returns this argument's `string` value prepended appropriately with its
+     * leading {@link prefixString} respective to whether it's an operand, flag,
+     * or option.
+     *
+     * If this argument is an operand and/or the {@link prefixString} is an
+     * empty `string` (`""`), then this object's {@link nonPrefixedString} is
+     * returned.
+     *
+     * If this argument is a flag, then this object's {@link nonPrefixedString}
+     * is returned prepended by a single leading instance of this object's
+     * {@link prefixString}.
+     *
+     * If this argument is an option, then this object's
+     * {@link nonPrefixedString} is returned prepended by a sequence of 2
+     * adjacent instances of this object's {@link prefixString}.
+     *
      * @public
      * @readonly
      */
-    public readonly prefixed: string;
+    public readonly prefixedString: string;
 
     /**
+     * Returns the value used to specify whether this argument is an operand,
+     * flag, or option.
+     *
      * @public
      * @readonly
      */
@@ -40,10 +62,23 @@ export class StringArgument
     readonly #flagString: string;
     readonly #optionString: string;
 
-    public constructor(prefixString: string, notPrefixedString: string, cliofoType: CliofoType)
+    /**
+     * Constructs a new object instance used to represent a command line
+     * argument as an operand, flag, or option.
+     *
+     * @param prefixString The `string` used to denote this argument as a flag
+     *                     or option (or operand).
+     *
+     * @param nonPrefixedString This argument's `string` value excluding its
+     *                          leading prefix `string`.
+     *
+     * @param cliofoType The operand, flag, or option type of command line
+     *                   argument this argument is.
+     */
+    public constructor(prefixString: string, nonPrefixedString: string, cliofoType: CliofoType)
     {
         this.prefixString = prefixString;
-        this.notPrefixed = notPrefixedString;
+        this.nonPrefixedString = nonPrefixedString;
         this.#optionPrefixString = this.prefixString.length === 0 ? this.prefixString
             : this.prefixString.repeat(2);
         this.cliofoType = cliofoType;
@@ -64,27 +99,76 @@ export class StringArgument
         this.#isFlag = this.cliofoType === CliofoType.FLAG;
         this.#isOption = this.cliofoType === CliofoType.OPTION;
 
-        this.prefixed =  this.prefixString.length === 0 || this.#isOperand
-            ? this.notPrefixed : (this.#prefix += this.notPrefixed);
+        this.prefixedString =  this.prefixString.length === 0 || this.#isOperand
+            ? this.nonPrefixedString : (this.#prefix += this.nonPrefixedString);
 
         this.#flagString = this.prefixString.length === 0 || this.#isFlag
-            ? this.prefixed : (this.prefixString += this.notPrefixed);
+            ? this.prefixedString : (this.prefixString += this.nonPrefixedString);
 
         this.#optionString = this.prefixString.length === 0 || this.#isOption
-            ? this.prefixed
-            : `${this.prefixString.repeat(2)}${this.notPrefixed}`;
+            ? this.prefixedString
+            : `${this.prefixString.repeat(2)}${this.nonPrefixedString}`;
     }
 
+    /**
+     * Returns this object's prefix `string` respective to what
+     * {@link cliofoType} it is.
+     *
+     * If it's an operand, then an empty `string` (`""`) is returned.
+     *
+     * If it's a flag, then this object's {@link prefixString} is returned.
+     *
+     * If it's an option, then this object's {@link prefixString} concatenated
+     * to itself and returned.
+     *
+     * @returns This object's prefix `string` respective to what
+     *          {@link cliofoType} it is.
+     */
     public prefix(): string { return this.#prefix; }
 
+    /**
+     * Returns this object's {@link nonPrefixedString} prepended with this
+     * object's {@link prefixString}.
+     *
+     * @returns This object's {@link nonPrefixedString} prepended with this
+     *          object's {@link prefixString}.
+     */
     public asFlagString(): string { return this.#flagString; }
 
+    /**
+     * Returns this object's {@link nonPrefixedString} prepended with 2
+     * adjacent instances of this object's {@link prefixString}.
+     *
+     * @returns This object's {@link nonPrefixedString} prepended with 2
+     *          adjacent instances of this object's {@link prefixString}.
+     */
     public asOptionString(): string { return this.#optionString; }
 
+    /**
+     * Returns `true` if this object's {@link cliofoType} is a
+     * {@link CliofoType.OPERAND}.
+     *
+     * @returns `true` if this object's {@link cliofoType} is a
+     *          {@link CliofoType.OPERAND}.
+     */
     public isOperand(): boolean { return this.#isOperand; }
 
+    /**
+     * Returns `true` if this object's {@link cliofoType} is a
+     * {@link CliofoType.FLAG}.
+     *
+     * @returns `true` if this object's {@link cliofoType} is a
+     *          {@link CliofoType.FLAG}.
+     */
     public isFlag(): boolean { return this.#isFlag; }
 
+    /**
+     * Returns `true` if this object's {@link cliofoType} is a
+     * {@link CliofoType.OPTION}.
+     *
+     * @returns `true` if this object's {@link cliofoType} is a
+     *          {@link CliofoType.OPTION}.
+     */
     public isOption(): boolean { return this.#isOption; }
 }
 
