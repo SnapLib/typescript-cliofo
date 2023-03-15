7299cfd4c3fbf50e916810ef92f4106d7e1699ff
Snap <XxSnapperGeexX@gmail.com>
2023-03-14 20:04:16 -0700

    Add and fisxup doc comments in StringConsumer class.

--- src/main/ts/lib/argument/consumer/string-consumer.ts
+++ src/main/ts/lib/argument/consumer/string-consumer.ts
@@ -4,8 +4,8 @@ import {StringArgument} from "../string-argument.js";
 
 /**
  * A string that can consume or is required to consume a range of 0 or more
- * `string` arguments and can optionally contain a predicate used to validate
- * consumed strings.
+ * `string` arguments and can optionally contain a `string` predicate used to
+ * validate consumed strings.
  */
 export class StringConsumer extends StringArgument
 {
@@ -30,12 +30,40 @@ export class StringConsumer extends StringArgument
 
     readonly #stringPredicate: (aString: string) => boolean;
 
+    /**
+     * Constructs an instance of an object used to represent a `string` that can
+     * consume or is required to consume a range of 0 or more `string` arguments
+     * and can optionally contain a `string` predicate used to validate consumed
+     * strings.
+     *
+     * @param prefixString The leading prefix `string` used to denote the
+     *                     constructed consumer as an operand, flag, or option.
+     *
+     * @param nonPrefixedString The `string` value of the constructed consumer
+     *                          excluding any prefix characters.
+     *
+     * @param cliofoType The operand, option, or flag type of the constructed
+     *                   consumer.
+     *
+     * @param cliofoTypeToConsume The operand, option, or flag type the
+     *                            constructed consumer consumes.
+     *
+     * @param range The minimum and max ranges the constructed consumer is
+     *              required to and can consume.
+     *
+     * @param stringPredicate The `string` predicate that can be used to
+     *                        validate consumed `string` arguments.
+     *
+     * @throws {ConsumerRangeError} If passed minimum and maximum range values
+     *                              aren't valid. Such as a min range that's
+     *                              greater than a max range.
+     */
     public constructor( prefixString: string,
                         nonPrefixedString: string,
                         cliofoType: CliofoType,
                         cliofoTypeToConsume: CliofoType,
                         range: Partial<{min: number, max: number}> = {min: 0, max: 0},
-                        stringPredicate: (aString: string) => boolean  = StringConsumer.#defaultStringPredicate)
+                        stringPredicate: (aString: string) => boolean  = StringConsumer.#defaultStringPredicate )
     {
         super(prefixString, nonPrefixedString, cliofoType);
 
@@ -63,22 +91,41 @@ export class StringConsumer extends StringArgument
         this.#stringPredicate = stringPredicate;
     }
 
+    /**
+     * Getter for this object's `string` predicate property.
+     *
+     * @returns This object's `string` predicate property.
+     *
+     * @public
+     */
     public stringPredicate(): (aString: string) => boolean
         { return this.#stringPredicate; }
 
+    /**
+     * Returns true` if this object's `string` predicate property is not set to
+     * the default `string` predicate.
+     *
+     * @returns `true` if this object's `string` predicate property is not set
+     *           to the default `string` predicate.
+     *
+     * @public
+     */
     public hasStringPredicate(): boolean
         { return this.#stringPredicate !== StringConsumer.#defaultStringPredicate; }
 
     /**
-     * Uses this object's internal `string` predicate to determine if the passed
-     * `string` is valid.
+     * Returns the result of passing the provided `string` argument to this
+     * object's `string` validator.
      *
      * @remarks If no `string` validator is set, then all passed `string`
      *          arguments evaluate to `true`.
      *
-     * @param aString The `string` to validate.
+     * @param aString The `string` to pass to this object's `string` validator.
      *
-     * @returns `true` if the passed string argument is valid.
+     * @returns The result of passing the provided `string` argument to this
+     *          object's `string` validator.
+     *
+     * @public
      */
     public stringIsValid(aString: string): boolean
         { return this.#stringPredicate(aString); }
