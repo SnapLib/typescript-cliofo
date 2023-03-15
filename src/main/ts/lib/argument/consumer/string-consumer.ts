ad8441c0ba2347f88b391881a6e1c65821cf4dd2
Snap <XxSnapperGeexX@gmail.com>
2023-03-14 22:49:23 -0700

    Add ctor overload to StringConsumer class.

--- src/main/ts/lib/argument/consumer/string-consumer.ts
+++ src/main/ts/lib/argument/consumer/string-consumer.ts
@@ -34,7 +34,7 @@ export class StringConsumer extends StringArgument
      * @public
      * @readonly
      */
-    public readonly range: Readonly<{min: number, max: number}>;
+    public readonly range: Readonly<Range>;
 
     static readonly #defaultRange: Readonly<{min: number, max: number}> = Object.freeze({min: 0, max: 0});
 
@@ -119,16 +119,71 @@ export class StringConsumer extends StringArgument
                         nonPrefixedString: string,
                         cliofoType: CliofoType,
                         cliofoTypeToConsume: CliofoType,
-                        range: Partial<{min: number, max: number}> = StringConsumer.#defaultRange,
-                        stringPredicate: (aString: string) => boolean  = StringConsumer.#defaultStringPredicate )
+                        range?: Partial<Range>,
+                        stringPredicate?: (aString: string) => boolean );
+
+    /**
+     * Constructs an instance of an object used to represent a `string` that can
+     * consume or is required to consume a range of 0 or more `string` arguments
+     * and can optionally contain a `string` predicate used to validate a
+     * `string`.
+     *
+     * If the number of arguments to consume is set to `0` then the constructed
+     * object won't consume any `string` arguments. Conversely, if set to
+     * `Infinity`, then an the constructed object can consume an indefinite
+     * amount of `string` arguments. Setting this number below 0 or greater than
+     * `Infinity` will cause an error to be thrown.
+     *
+     * The `string` predicate defaults to a method that doesn't consume any
+     * arguments and returns `false`.
+     *
+     * @param prefixString The leading prefix `string` used to denote the
+     *                     constructed consumer as an operand, flag, or option.
+     *
+     * @param nonPrefixedString The `string` value of the constructed consumer
+     *                          excluding any prefix characters.
+     *
+     * @param cliofoType The operand, option, or flag type of the constructed
+     *                   consumer.
+     *
+     * @param cliofoTypeToConsume The operand, option, or flag type the
+     *                            constructed consumer consumes.
+     *
+     * @param numberOfArgumentsToConsume The `number` of `string` arguments the
+     *     constructed object will require to consume.
+     *
+     * @param stringPredicate a `string` predicate that can be used to validate
+     *                        `string` arguments.
+     *
+     * @throws {ConsumerRangeError} If minimum and maximum range values aren't
+     *                              valid. Such as a min range that's greater
+     *                              than a max range.
+     */
+    public constructor( prefixString: string,
+                        nonPrefixedString: string,
+                        cliofoType: CliofoType,
+                        cliofoTypeToConsume: CliofoType,
+                        numberOfArgumentsToConsume?: number,
+                        stringPredicate?: (aString: string) => boolean );
+    constructor( prefixString: string,
+                 nonPrefixedString: string,
+                 cliofoType: CliofoType,
+                 cliofoTypeToConsume: CliofoType,
+                 rangeOrNumber: Partial<Range> | number = StringConsumer.#defaultRange,
+                 stringPredicate: (aString: string) => boolean  = StringConsumer.#defaultStringPredicate )
     {
         super(prefixString, nonPrefixedString, cliofoType);
 
-        const minRange: number = range.min ?? 0;
+        const minRange: number = typeof rangeOrNumber === "number" ? rangeOrNumber : rangeOrNumber.min  ?? 0 ;
 
         // if min and max range are undefined, set max range to 0, otherwise
         // set to infinity if only max range is undefined
-        const maxRange: number = range.max ?? (range.min === undefined || range.min === null ? 0 : Infinity);
+        const maxRange: number = typeof rangeOrNumber === "number"
+            ? rangeOrNumber
+            : rangeOrNumber.max ?? ( rangeOrNumber.min === undefined
+                                     || rangeOrNumber.min === null
+                                        ? 0
+                                        : Infinity);
 
         if (minRange >= Infinity)
         {
@@ -211,6 +266,8 @@ export class StringConsumer extends StringArgument
     protected static defaultStringPredicate(): () => boolean
         { return StringConsumer.#defaultStringPredicate; }
 }
+
+type Range = {readonly min: number; readonly max: number;};
 
 export {StringConsumer as default};
 
