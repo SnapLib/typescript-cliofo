72c4845a64d0c78da3c53fbd8702987e86adf9a9
Snap <XxSnapperGeexX@gmail.com>
2023-03-19 19:45:44 -0700

    Begin creating internal TypeConsumerParaer properties.

--- src/main/ts/lib/type-consumer-parser.ts
+++ src/main/ts/lib/type-consumer-parser.ts
@@ -1,18 +1,46 @@
 import {TypeConsumer} from "./argument/consumer/type-consumer.js";
+import {CliofoType} from "./cliofo-type.js";
 import {Parser} from "./parser.js";
 
 /**
  * An object that consume a {@link Parser} object and coverts it to a set of
- * parsed {@link TypeConsumer} objects.
+ * parsed {@link TypeConsumer} objects unique to each of their argument indexes..
  */
-export class StringConsumerParser
+export class StringConsumerParser<ConvertedStringType>
 {
     public readonly parser: Readonly<Parser>;
 
-    public constructor(parser: Readonly<Parser>)
+    public readonly parsedTypeConsumers: ReadonlySet<ParsedTypeConsumer<ConvertedStringType>>;
+
+    public readonly operands: ReadonlySet<TypeConsumer<ConvertedStringType>>;
+
+    public constructor(
+        parser: Readonly<Parser>,
+
+        stringPredicate: (aString: string) => boolean,
+
+        stringConverter: (aString: string) => ConvertedStringType,
+
+        convertedStringPredicate: (convertedString: ConvertedStringType) => boolean)
     {
-        this.parser = Object.isFrozen(parser) ? parser : Object.freeze(parser);
+        this.parser = Object.isFrozen(parser) ? parser : Object.freeze(new Parser(parser));
+
+        this.parsedTypeConsumers = Parser.arguments.length === 0
+            ? Object.freeze(new Set<ParsedTypeConsumer<ConvertedStringType>>())
+            : new Set();
+
+        this.operands = new Set([...this.parser.operand.indexes.entries()].map(operandIndexEntry => new TypeConsumer<ConvertedStringType>(
+            this.parser.prefixString,
+            operandIndexEntry[0],
+            CliofoType.OPERAND,
+            {min: 0, max: 0},
+            new Set(),
+            stringPredicate,
+            stringConverter,
+            convertedStringPredicate
+        )));
     }
 }
+ type ParsedTypeConsumer<ConvertedStringType> = TypeConsumer<ConvertedStringType> & {readonly index: number};
 
 export {StringConsumerParser as default};
