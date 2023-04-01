f7759a31f811bb68fcb3ed036bc15476699eaad5
Snap <XxSnapperGeexX@gmail.com>
2023-03-31 23:36:36 -0700

    Add doc comment to the CliofoType enum.

--- src/main/ts/lib/cliofo-type.ts
+++ src/main/ts/lib/cliofo-type.ts
@@ -1,3 +1,28 @@
+/**
+ * The 3 different types of command line interface arguments. These consist of
+ * the:
+ *
+ * 1. ***OPERAND***
+ *
+ * Any argument that isn't prefixed with a leading string designating it a
+ * ***FLAG*** or an ***OPTION***.
+ *
+ * 1. ***FLAG***
+ *
+ * If an argument is prefixed with only a single leading instance of a leading
+ * string, it's interpreted as a ***FLAG***.
+ *
+ * 1. ***OPTION***
+ *
+ * If an argument is prefixed with a sequence of 2 or more instances of a
+ * leading prefix string, then it's interpreted as an ***OPTION***.
+ *
+ * The leading prefix string can be any string. If the leading prefix string
+ * is an empty string (`""`), then every argument is interpreted as an
+ * ***OPERAND*** since there is no leading string.
+ *
+ * @enum
+ */
 export enum CliofoType
 {
     OPERAND = "OPERAND",
