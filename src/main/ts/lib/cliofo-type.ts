d1b777526f11ce8a2cc67836e92fca778b3baf1d
Snap <XxSnapperGeexX@gmail.com>
2023-04-01 00:28:45 -0700

    Add to doc comment of CliofoTyoe enum.

--- src/main/ts/lib/cliofo-type.ts
+++ src/main/ts/lib/cliofo-type.ts
@@ -1,6 +1,7 @@
 /**
- * The 3 different types of command line interface arguments. These consist of
- * the:
+ * The 3 different types of command line interface arguments. A specified
+ * `string` is used to denote if other `string`s are*** OPERANDS***,
+ * ***FLAGS***, or ***OPTIONS***:
  *
  * 1. ***OPERAND***
  *
@@ -9,19 +10,45 @@
  *
  * 1. ***FLAG***
  *
- * If an argument is prefixed with only a single leading instance of a leading
- * string, it's interpreted as a ***FLAG***.
+ * Any argument that is prefixed with only a single leading instance of a
+ * string designating it a ***FLAG*** an ***OPTION*** is interpreted as a
+ * ***FLAG***.
  *
  * 1. ***OPTION***
  *
- * If an argument is prefixed with a sequence of 2 or more instances of a
- * leading prefix string, then it's interpreted as an ***OPTION***.
+ * Any argument that is prefixed with a leading sequence of 2 or more instances
+ * of a string designating it a ***FLAG*** or an ***OPTION*** is interpreted as
+ * an ***OPTION***.
  *
  * The leading prefix string can be any string. If the leading prefix string
  * is an empty string (`""`), then every argument is interpreted as an
  * ***OPERAND*** since there is no leading string.
  *
- * @enum
+ * @example
+ * If a single hyphen character (`"-"`) is the leading prefix string used to
+ * designate if an argument is a ***FLAG*** or ***OPTION***:
+ *
+ * ```shell
+ * $node my-script.js --anOption -aFlag anOperand
+ * ```
+ *
+ * In the above example, the 3 arguments "--anOption", "--aFlag", and
+ * "anOperand" are being passed to the javascript script "my-script.js" being
+ * executed by node.
+ *
+ * The 1st argument, "--anOption", is interpreted as an option since it's
+ * prefixed with at least 2 instances of the leading prefix string which in this
+ * case is a dash character (`"-"`).
+ *
+ * The 2nd argument, "--aFlag", is interpreted as a flag since it's
+ * prefixed with only a single instance of the leading prefix string which in
+ * this case is a dash character (`"-"`).
+ *
+ * The 3rd argument, "--anOperand", is interpreted as an operand since it's
+ * prefixed with no instances of the leading prefix string which in
+ * this case is a dash character (`"-"`).
+ *
+ * @enum {string}
  */
 export enum CliofoType
 {
