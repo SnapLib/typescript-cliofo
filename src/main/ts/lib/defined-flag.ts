2f14c597e54af343ebcdf07e950804047e3ed943
Snap <XxSnapperGeexX@gmail.com>
2023-07-07 22:43:18 -0700

    Make string parameters of DefinedFlag nonnullable and make factory method for DefinedFlag.

--- src/main/ts/lib/defined-flag.ts
+++ src/main/ts/lib/defined-flag.ts
@@ -1,11 +1,12 @@
-import { UndefinedFlag } from "./undefined-flag.js";
+import { type DefinedArgument } from "./defined-argument.js";
+import { UndefinedFlag, undefinedFlag } from "./undefined-flag.js";
 
-export class DefinedFlag extends UndefinedFlag
+export class DefinedFlag extends UndefinedFlag implements DefinedArgument
 {
     readonly #description: string;
     readonly #name: string;
 
-    public constructor(undefinedFlag: UndefinedFlag, description: string, name: string)
+    public constructor(undefinedFlag: UndefinedFlag, description: NonNullable<string>, name: NonNullable<string>)
     {
         super(undefinedFlag);
         this.#description = description;
@@ -24,4 +25,20 @@ export class DefinedFlag extends UndefinedFlag
     }
 }
 
-//TODO Add factory method.
+export function definedFlag(prefixString: string, valueString: string, description?: string, name?: string): DefinedFlag;
+export function definedFlag(prefixCodePoint: number, valueString: string, description?: string, name?: string): DefinedFlag;
+export function definedFlag(undefinedFlag: UndefinedFlag, description?: string, name?: string): DefinedFlag;
+export function definedFlag(prefixStringOrUndefinedFlag: string | number | UndefinedFlag, valueStringOrDescription?: string, nameOrDescription?: string, name?: string): DefinedFlag
+{
+    if (typeof prefixStringOrUndefinedFlag === "string")
+    {
+        return new DefinedFlag(undefinedFlag(prefixStringOrUndefinedFlag, valueStringOrDescription ?? ""), nameOrDescription ?? "", name ?? "");
+    }
+
+    if (typeof prefixStringOrUndefinedFlag === "number")
+    {
+        return new DefinedFlag(undefinedFlag(prefixStringOrUndefinedFlag, valueStringOrDescription ?? ""), nameOrDescription ?? "", name ?? "");
+    }
+
+    return new DefinedFlag(prefixStringOrUndefinedFlag, valueStringOrDescription ?? "", nameOrDescription ?? "");
+}
