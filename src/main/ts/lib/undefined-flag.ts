71a1605eb7ece271cb50549a5f26737be9b26b29
Snap <XxSnapperGeexX@gmail.com>
2023-07-09 05:38:28 -0700

    Add doc comments to flag prefix and value predicate functions in undefined-flags.

--- src/main/ts/lib/undefined-flag.ts
+++ src/main/ts/lib/undefined-flag.ts
@@ -3,15 +3,41 @@ import { ArgumentString, argumentString } from "./argument-string.js";
 import { ConstrainedArgumentString } from "./constrained-argument-string.js";
 import { PrefixConstraintViolationError } from "./error/prefix-constraint-violation-error.js";
 
+/**
+ * Predicate that consumes a `string` and returns `true` if it's not `undefined`
+ * or `null` and only consists of a single character.
+ *
+ * This is used to validate the prefixes for flag arguments.
+ *
+ * @param flagPrefixString The `string` being checked if it's a valid flag prefix.
+ *
+ * @returns `true` if the passed string isn't `undefined` or `null` and contains
+ *         only a single character.
+ */
 export const flagPrefixPredicate: stringPredicate =
-    Object.freeze(
-        (flagPrefixString: string) =>    flagPrefixString !== undefined
-                                      && flagPrefixString !== null
-                                      && flagPrefixString.length === 1
-    );
+    Object.freeze(function(flagPrefixString: string): boolean
+    {
+        return    flagPrefixString !== undefined
+               && flagPrefixString !== null
+            && flagPrefixString.length === 1;
+    });
 
+/**
+ * Predicate that consumes 2 `string`s and returns `true` if the second `string`
+ * is empty or doesn't start with the first `string` argument.
+ *
+ * This is used to validate the values (the part that comes after the prefix)
+ * for flags.
+ *
+ * @param flagPrefixString The flag prefix `string` used to validate the flag value.
+ *
+ * @param flagValueString The flag value `string` being validated.
+ *
+ * @returns `true` if the passed flag `string` value is empty or doesn't start
+ *          with the flag prefix `string`.
+ */
 export const flagValuePredicate: biStringPredicate =
-    Object.freeze( (flagPrefixString: string, flagValueString: string) =>
+    Object.freeze( function(flagPrefixString: string, flagValueString: string)
     {
         // allow empty strings and strings that don't contain leading prefix string
         return flagValueString.length === 0 || flagValueString.slice(0, flagPrefixString.length) !== flagPrefixString;
