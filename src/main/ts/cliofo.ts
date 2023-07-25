fdb8841d222c92915d5fff42163dc61f330c1509
Snap <XxSnapperGeexX@gmail.com>
2023-07-25 01:34:10 -0700

    Use ArgCollection class isntead of PrefixParser class in cliofo entry point.

--- src/main/ts/cliofo.ts
+++ src/main/ts/cliofo.ts
@@ -1,6 +1,10 @@
-import path from "node:path";
-import {fileURLToPath} from "node:url";
+import { optionString } from "./lib/argument/option-string.js";
+import { ArgCollection } from "./lib/arg-collection.js";
+import { flagString } from "./lib/argument/flag-string.js";
 
-const __dirname: string = path.dirname(fileURLToPath(import.meta.url));
+const argCollection = new ArgCollection([
+    flagString("-", "a"),
+    optionString("--", "directory")
+]);
 
-console.log(path.resolve("./blep"));
+console.log(argCollection.parseIndexes(process.argv.slice(2)));
