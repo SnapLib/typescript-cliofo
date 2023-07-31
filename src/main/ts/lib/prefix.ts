c1a3377d632f3aeba0783bbb4d9d9593f8256045
Snap <XxSnapperGeexX@gmail.com>
2023-07-30 20:32:49 -0700

    Add copy constructor and factory method for Prefix class.

--- src/main/ts/lib/prefix.ts
+++ src/main/ts/lib/prefix.ts
@@ -6,29 +6,36 @@ export class Prefix
     readonly #optionString: string;
     readonly #string: string;
 
-    public constructor(flagCharOrCodePoint: NonNullable<string | number>)
+    public constructor(flagCharOrCodePointOrOther: NonNullable<string | number | Prefix>)
     {
-        if (flagCharOrCodePoint === undefined || flagCharOrCodePoint === null)
+        if (flagCharOrCodePointOrOther === undefined || flagCharOrCodePointOrOther === null)
         {
             throw new Error("undefined or null flag char or code point.");
         }
 
-        if (typeof flagCharOrCodePoint === "string")
+        if (typeof flagCharOrCodePointOrOther === "string")
         {
-            if (flagCharOrCodePoint.length !== 1)
+            if (flagCharOrCodePointOrOther.length !== 1)
             {
-                throw new Error(`Flag character doesn't consist of single character: "${flagCharOrCodePoint}"`);
+                throw new Error(`Flag character doesn't consist of single character: "${flagCharOrCodePointOrOther}"`);
             }
 
-            this.#flagChar = flagCharOrCodePoint;
+            this.#flagChar = flagCharOrCodePointOrOther;
+            this.#optionString = this.#flagChar.repeat(2);
+            this.#string = `${this.constructor.name} {flagChar: '${this.#flagChar}', optionString: "${this.#optionString}"}`;
+        }
+        else if (typeof flagCharOrCodePointOrOther === "number")
+        {
+            this.#flagChar = String.fromCodePoint(flagCharOrCodePointOrOther);
+            this.#optionString = this.#flagChar.repeat(2);
+            this.#string = `${this.constructor.name} {flagChar: '${this.#flagChar}', optionString: "${this.#optionString}"}`;
         }
         else
         {
-            this.#flagChar = String.fromCodePoint(flagCharOrCodePoint);
+            this.#flagChar = flagCharOrCodePointOrOther.#flagChar;
+            this.#optionString = flagCharOrCodePointOrOther.#optionString;
+            this.#string = flagCharOrCodePointOrOther.#string;
         }
-
-        this.#optionString = this.#flagChar.repeat(2);
-        this.#string = `${this.constructor.name} {flagChar: '${this.#flagChar}', optionString: "${this.#optionString}"}`;
     }
 
     public get flagChar(): string { return this.#flagChar; }
@@ -41,9 +48,10 @@ export class Prefix
 
 export function prefix(flagChar: NonNullable<string>): Readonly<Prefix>;
 export function prefix(flagCodePoint: NonNullable<number>): Readonly<Prefix>;
-export function prefix(flagCharOrCodePoint: NonNullable<string | number>): Readonly<Prefix>
+export function prefix(otherPrefix: NonNullable<Prefix>): Readonly<Prefix>;
+export function prefix(flagCharOrCodePointOrOther: NonNullable<string | number | Prefix>): Readonly<Prefix>
 {
-    return Object.freeze(new Prefix(flagCharOrCodePoint));
+    return Object.freeze(new Prefix(flagCharOrCodePointOrOther));
 }
 
 
