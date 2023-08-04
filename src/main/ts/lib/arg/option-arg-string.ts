0ffc7e6c33502043f38a26f7e14e0858dd3dcbd2
Snap <XxSnapperGeexX@gmail.com>
2023-08-04 00:34:20 -0700

    Init OptionArgString class with implementation.

---
+++ src/main/ts/lib/arg/option-arg-string.ts
@@ -0,0 +1,25 @@
+import { argStringConstraint, type ArgStringConstraint, type PrefixPredicate, type ValuePredicate } from "./arg-string-constraint.js";
+import { ConstrainedArgString } from "./constrained-arg-string.js";
+import { StringPrefixArgString, stringPrefixArgString as stringPrefixArgString } from "./string-prefix-arg-string.js";
+
+ export const optionPrefixPredicate: PrefixPredicate<string> =
+    Object.freeze( (prefixString?: string): boolean =>
+           prefixString !== undefined
+        && prefixString !== null
+        && prefixString.length > 1 );
+
+export const optionValuePredicate: ValuePredicate<string> =
+    Object.freeze( (prefixString: NonNullable<string>, valueString: string) =>
+           valueString !== undefined && valueString !== null );
+
+export const flagArgStringConstraint: ArgStringConstraint<string> = argStringConstraint(optionPrefixPredicate, optionValuePredicate);
+
+export class OptionArgString extends ConstrainedArgString<string>
+{
+    public constructor(stringPrefixArgumentString: NonNullable<StringPrefixArgString>)
+    {
+        super(flagArgStringConstraint, Object.isFrozen(stringPrefixArgumentString) ? stringPrefixArgumentString : Object.freeze(stringPrefixArgString(stringPrefixArgumentString)));
+    }
+}
+
+export {OptionArgString as default};
