9378ab8e5fd011f6f9dfc21c6a618cd52849000a
Snap <XxSnapperGeexX@gmail.com>
2023-08-04 23:49:43 -0700

    Init OperandArgString class with implementation.

---
+++ src/main/ts/lib/arg/operand-arg-string.ts
@@ -0,0 +1,44 @@
+import { argStringConstraint, type ArgStringConstraint, type PrefixPredicate, type ValuePredicate } from "./arg-string-constraint.js";
+import { ConstrainedArgString } from "./constrained-arg-string.js";
+import { StringSetPrefixArgString, stringSetPrefixArgString as stringSetPrefixArgString } from "./string-set-prefix-arg-string.js";
+
+const whiteSpaceRegEx: Readonly<RegExp> = Object.freeze(/\s/g);
+
+ export const operandPrefixPredicate: PrefixPredicate<ReadonlySet<string>> =
+    Object.freeze( (prefixStringSet?: ReadonlySet<string>): boolean =>
+    {
+        if (prefixStringSet === undefined || prefixStringSet === null || prefixStringSet.size === 0) { return false; }
+
+        for (const prefixString of prefixStringSet)
+        {
+            if (prefixString.length === 0 || whiteSpaceRegEx.test(prefixString)) { return false; }
+        }
+
+        return true;
+    });
+
+export const operandValuePredicate: ValuePredicate<ReadonlySet<string>> =
+    Object.freeze( (prefixStringSet: NonNullable<ReadonlySet<string>>, valueString: string): boolean =>
+    {
+        if (valueString === undefined || valueString === null || valueString.length === 0) { return false; }
+        if (whiteSpaceRegEx.test(valueString)) { return false; }
+
+        for (const prefixString of prefixStringSet)
+        {
+            if (prefixString.length > 0 && valueString.startsWith(prefixString)) { return false; }
+        }
+
+        return true;
+    });
+
+export const operandArgStringConstraint: ArgStringConstraint<ReadonlySet<string>> = argStringConstraint(operandPrefixPredicate, operandValuePredicate);
+
+export class OperandArgString extends ConstrainedArgString<ReadonlySet<string>>
+{
+    public constructor(stringSetPrefixArgumentString: NonNullable<StringSetPrefixArgString>)
+    {
+        super(operandArgStringConstraint, Object.isFrozen(stringSetPrefixArgumentString) ? stringSetPrefixArgumentString : Object.freeze(stringSetPrefixArgString(stringSetPrefixArgumentString)));
+    }
+}
+
+export {OperandArgString as default};
