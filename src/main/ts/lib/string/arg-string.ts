18d8e92b504b849285c99b42b22fcbe3e4516763
Snap <XxSnapperGeexX@gmail.com>
2023-08-11 01:54:00 -0700

    Add more doc comments to ArgString class.

--- src/main/ts/lib/string/arg-string.ts
+++ src/main/ts/lib/string/arg-string.ts
@@ -6,9 +6,9 @@ import { inspect } from "util";
  * {@link ConstrainedArgString} can consume. It consists of a `string` or
  * `ReadonlySet<string>` type.
  *
- * @see ArgString
- * @see ArgStringConstraint
- * @see ConstrainedArgString
+ * @see {@link ArgString}
+ * @see {@link ArgStringConstraint}
+ * @see {@link ConstrainedArgString}
  */
 export type StringOrReadonlyStringSet = string | ReadonlySet<string>;
 
@@ -21,13 +21,31 @@ export type StringOrReadonlyStringSet = string | ReadonlySet<string>;
  * 1. and a suffix ***value*** `string` appended to the leading prefix
  *
  * The leading prefix can be a single `string` or a set of multiple `string`s if
- * a use case requires specifying more than 1 leading prefix `string`.
+ * a use case requires specifying more than 1 leading prefix `string`. This is
+ * utilized by this API's {@link OperandArgString} class which specifies multiple
+ * `string` values it's *not* allowed to have.
+ *
+ * @typeParam {StringOrReadonlyStringSet} PrefixType - `string` or
+ * `ReadonlySet<string>` type specifying what type of prefix this object has.
  */
 export abstract class ArgString<PrefixType extends StringOrReadonlyStringSet>
 {
     readonly #prefix: PrefixType;
     readonly #value: string;
 
+    /**
+     * Constructs an instance of an {@link ArgString} object. If `undefined` or
+     * `null` is passed for either argument, an error will be thrown.
+     *
+     * @param prefix The value to set at this object's prefix or prefixes.
+     *
+     * @param value The `string` to set as this object's suffix value.
+     *
+     * @throws {Error} if `undefined` or `null` is passed for either argument.
+     *
+     * @protected
+     * @constructor
+     */
     protected constructor(prefix: NonNullable<PrefixType>, value: NonNullable<string>)
     {
         if (prefix === undefined || prefix === null)
@@ -44,13 +62,66 @@ export abstract class ArgString<PrefixType extends StringOrReadonlyStringSet>
         this.#value = value;
     }
 
+    /**
+     * Getter for this object's leading `string` or `ReadonlySet<string>` prefix
+     * property.
+     *
+     * @returns this object's leading `string` or `ReadonlySet<string>` prefix property.
+     *
+     * @public
+     * @method
+     */
     public get prefix(): PrefixType { return this.#prefix; }
+
+    /**
+     * Getter for this object's suffix `string` value property.
+     *
+     * @returns this object's suffix `string` value property.
+     *
+     * @public
+     * @method
+     */
     public get value(): string { return this.#value; }
+
+    /**
+     * Getter for this object's `string` or `ReadonlySet<string>` prefixed value
+     * property.
+     *
+     * @returns this object's `string` or `ReadonlySet<string>` prefixed value property.
+     *
+     * @public
+     * @method
+     */
     public abstract get prefixedValue(): PrefixType;
 
+    /**
+     * Returns `true` if the passed argument is equal to this {@link ArgString} object.
+     *
+     * @param obj The argument being compared for equality to this
+     *            {@link ArgString} object.
+     *
+     * @returns `true` if the passed argument is equal to this {@link ArgString} object.
+     */
     public abstract equals(obj?: unknown): boolean;
+
+    /**
+     * Returns a `string` representation of this object.
+     *
+     * @returns a `string` representation of this object.
+     *
+     * @public
+     * @method
+     */
     public abstract toString(): string;
 
+    /**
+     * Returns a `string` representation of this object.
+     *
+     * @returns a `string` representation of this object.
+     *
+     * @public
+     * @method
+     */
     public abstract [inspect.custom](): string;
 }
 
