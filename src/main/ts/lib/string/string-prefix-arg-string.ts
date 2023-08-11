49a319b8d5f471519a6deb3a6dce50e11ad62ba0
Snap <XxSnapperGeexX@gmail.com>
2023-08-11 02:39:36 -0700

    Add some doc comments to StringPrefixArgString class.

--- src/main/ts/lib/string/string-prefix-arg-string.ts
+++ src/main/ts/lib/string/string-prefix-arg-string.ts
@@ -3,6 +3,18 @@ import { inspect } from "util";
 
 const stringToString = (aString: string) => aString.length != 1 ? `"${aString}"` : `'${aString}'`;
 
+/**
+ * This class is used to create objects that represent string arguments consisting
+ * of `string` prefixes prepended to `string` values that can be passed on the
+ * command line.
+ *
+ * @remarks
+ * This API's {@link FlagArgString} and {@link OptionArgString} classes inherit
+ * from this class as those classes can only have single `string` prefixes.
+ *
+ * @see {@link FlagArgString}
+ * @see {@link OptionArgString}
+ */
 export class StringPrefixArgString extends ArgString<string>
 {
     readonly #prefixedValue: string;
@@ -15,8 +27,26 @@ export class StringPrefixArgString extends ArgString<string>
         this.#string = `${StringPrefixArgString.name} {prefix: ${stringToString(super.prefix)}, value: ${stringToString(super.value)}}`;
     }
 
+    /**
+     * Returns this object's prefixed `string` value property consisting of this
+     * object's prefix `string` property prepended to this object's `string`
+     * value property.
+     *
+     * @returns this object's prefixed `string` value property.
+     */
     public override get prefixedValue(): string { return this.#prefixedValue; }
 
+    /**
+     * Returns `true` if the passed argument is a {@link StringPrefixArgString}
+     * object prefix and value `string` properties equal to this object's prefix
+     * and value `string` property.
+     *
+     * @param obj The argument being compared for equality to this
+     *            {@link StringPrefixArgString} object.
+     *
+     * @returns `true` if the passed argument is equal to this
+     *          {@link StringPrefixArgString} object.
+     */
     public override equals(obj?: unknown): boolean
     {
         return    this === obj || obj instanceof StringPrefixArgString
