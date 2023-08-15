684bb566290a1e480856175224796ea896970b6d
Snap <XxSnapperGeexX@gmail.com>
2023-08-15 09:59:48 -0700

    Add to and fixup doc comments in flag-arg-string module.

--- src/main/ts/lib/constrained-string/flag-arg-string.ts
+++ src/main/ts/lib/constrained-string/flag-arg-string.ts
@@ -1,16 +1,33 @@
 /**
- * This module contains all the necessary components to create an object to represent a GNU *flag* argument.
+ * This module contains all the necessary components to create an object to represent a GNU *flag* argument. GNU flag
+ * arguments are generally prefixed with a single dash character and consist of a single character. Multiple flag
+ * characters can be combined into a single argument.Examples of flag arguments being passed to a script are:
+ *
+ * ```bash
+ * node-script.js -l -aC
+ * ```
+ *
+ * `node-script.js` is the script being passed the flag arguments `l`, `a`, and `C`.
  *
  * ## Prefix and value `string` predicates
  *
  * 1. {@link flagPrefixPredicate}
  *
- *     This predicate ensures that a `string` is only 1 character and not a whitespace.
+ *     This predicate is used to validate the prefix character of a flag (the dash, `-`, in the above example) and
+ *      ensures that a `string` is:
+ *     - only 1 character
+ *     - and not a whitespace.
  *
  * 1. {@link flagValuePredicate}
  *
- *     This predicate ensures that a `string` is 0-1 character(s) long, is not a specified character,
- *     and does not contain any white space.
+ *     This predicate is used to validate the character value of a flag (the `l`, `a`, and `C` characters in the above
+ *     example) and consumes 2 `string`s and ensures that the 2nd `string` argument is:
+ *     - 0-1 character(s)
+ *     - not equal to the 1st string argument
+ *     - and is not a whitespace.
+ *
+ *     The first `string` argument this predicate consumes is meant to be the prefix character to make sure it doesn't
+ *     clash with the value character.
  *
  * ## {@link flagArgStringConstraint}
  *
@@ -18,13 +35,13 @@
  *
  * ## {@link FlagArgString}
  *
- * The class bringing all the above components together combined with a {@link string-prefix-arg-string.StringPrefixArgString}
- * for creating an object used to represent a GNU *flag* argument.
+ * All the above components combined and used in conjunction with a {@link string-prefix-arg-string.StringPrefixArgString}
+ * object creates the object used to represent a GNU *flag* argument.
  *
  * @module flag-arg-string
  *
  * @see {@link constrained-arg-string}
- * @see {@link string-prefix-arg}
+ * @see {@link string-prefix-arg-string}
  */
 
 import { argStringConstraint, type ArgStringConstraint, type PrefixPredicate, type ValuePredicate } from "./arg-string-constraint.js";
@@ -34,15 +51,17 @@ import { StringPrefixArgString, stringPrefixArgString } from "../string/string-p
 const whiteSpaceRegEx: Readonly<RegExp> = /\s/g;
 
 /**
- * Predicate that consumes a `string` and returns `true` if it's not `undefined`
- * or `null`, only consists of a single character, and is not a whitespace.
+ * Predicate that consumes a `string` and returns `true` if it's:
+ * - not `undefined` or `null`
+ * - only a single character
+ * - and is not a whitespace.
  *
- * This is used to validate the prefixes (the leading part prepended to the value) of flag arguments.
+ * This is used to validate the prefix (the leading part prepended to the value) of a flag argument.
  *
  * @param prefixString The `string` being checked if it's a valid flag prefix.
  *
- * @returns `true` if the passed `string` isn't `undefined` or `null`, only
- * consists of a single character, and is not a whitespace.
+ * @returns `true` if the passed `string` isn't `undefined` or `null`, only consists of a single character, and is not a
+ *          whitespace.
  *
  * @see FlagArgString
  */
@@ -54,22 +73,23 @@ const whiteSpaceRegEx: Readonly<RegExp> = /\s/g;
         && ! whiteSpaceRegEx.test(prefixString) );
 
 /**
- * Predicate that consumes 2 `string`s and returns `true` if the second `string`
- * argument is 0 or 1 characters, doesn't start with the first `string`, and
- * doesn't contain any whitespace.
+ * Predicate that consumes 2 `string`s and returns `true` if the 2nd `string` argument is:
+ * - not `undefined` or `null`
+ * - 0-1 character(s)
+ * - not equal to the 1st string argument
+ * - and is not a whitespace.
  *
- * This is used to validate the value (the part that's appended to the prefix)
- * of flag arguments.
+ * This is used to validate the value (the part that's appended to the prefix) of a flag argument. The first `string`
+ * argument it consumes is intended be the prefix of the flag argument to ensure it doesn't clash with the value.
  *
- * This function does not perform any validation on the first `string` argument
- * it's passed.
+ * This function does not perform any validation on the first `string` argument, the prefix, it's passed.
  *
- * @param prefixString The prefix `string` used to validate the flag value.
+ * @param prefixString The flag prefix `string` used to validate the flag value.
  *
  * @param valueString The flag value `string` being validated.
  *
- * @returns `true` if the second `string` argument is 0 or 1 characters, doesn't
- *          start with the first `string`, and doesn't contain any whitespace.
+ * @returns `true` if the second `string` argument is 0 or 1 characters, doesn't start with the first `string`, and
+ *          doesn't contain any whitespace.
  *
  * @see FlagArgString
  */
@@ -82,12 +102,26 @@ export const flagValuePredicate: ValuePredicate<string> =
         && ! whiteSpaceRegEx.test(valueString) );
 
 /**
- * Object containing the {@link flagPrefixPredicate} and {@link flagValuePredicate}
- * `string` predicates for validating the {@link StringPrefixArgString.prefix}
- * and {@link StringPrefixArgString.value} of a {@link StringPrefixArgString}.
+ * Object containing the {@link flagPrefixPredicate} and {@link flagValuePredicate} `string` predicates for validating
+ * the {@link StringPrefixArgString.prefix} and {@link StringPrefixArgString.value} of a {@link StringPrefixArgString}.
  */
 export const flagArgStringConstraint: ArgStringConstraint<string> = argStringConstraint(flagPrefixPredicate, flagValuePredicate);
 
+/**
+ * The class used to construct object instances to represent GNU *flag* arguments. It consists of the:
+ * - {@link flagPrefixPredicate} to ensure its prefix is a single non-whitespace character.
+ * - {@link flagValuePredicate} to ensure its value is a single non-whitespace character that's not equal to its prefix.
+ * - {@link StringPrefixArgString} to contain its prefix and value `string`s.
+ * - an optional name `string`.
+ *
+ * Below is an example of flag arguments being passed on the command line:
+ *
+ * ```bash
+ * node-script.js -l -aC
+ * ```
+ *
+ * `node-script.js` is the script being passed the flag arguments `l`, `a`, and `C`.
+ */
 export class FlagArgString extends ConstrainedArgString<string>
 {
     public constructor(stringPrefixArgumentString: NonNullable<StringPrefixArgString>, name?: string)
