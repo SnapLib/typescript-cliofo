8b6e68913ad9900024b2b017014d084c14b5bcf3
Snap <XxSnapperGeexX@gmail.com>
2023-08-22 13:33:52 -0700

    Begin adding unit tests for StringPrefixArgString.

---
+++ src/test/ts/lib/string/test-string-prefix-arg-string.spec.ts
@@ -0,0 +1,50 @@
+import { StringPrefixArgString } from "../../../../main/ts/lib/string/string-prefix-arg-string.js";
+import { ArgStringPrefixError,  ArgStringValueError } from "../../../../main/ts/lib/string/arg-string.js";
+import { assert } from "chai";
+import { suite, test } from "mocha";
+
+suite(`${StringPrefixArgString.name} class`, function testSuiteStringPrefixArgStringClass()
+{
+    suite(`${StringPrefixArgString.name} constructor`, function testStringPrefixArgStringConstructor()
+    {
+        test(`undefined prefix constructor argument throws ${ArgStringPrefixError.name}`, function testStringPrefixArgStringConstructor_ThrowsArgStringPrefixError_WhenPassedUndefinedPrefixArgument()
+        {
+            assert.throws(() => new StringPrefixArgString(undefined!, "Simba"), ArgStringPrefixError);
+        });
+
+        test(`null prefix constructor argument throws ${ArgStringPrefixError.name}`, function testStringPrefixArgStringConstructor_ThrowsArgStringPrefixError_WhenPassedNullPrefixArgument()
+        {
+            assert.throws(() => new StringPrefixArgString(null!, "Kion"), ArgStringPrefixError);
+        });
+
+        test(`undefined value constructor argument throws ${ArgStringValueError.name}`, function testStringPrefixArgStringConstructor_ThrowsArgStringValueError_WhenPassedUndefinedValueArgument()
+        {
+            assert.throws(() => new StringPrefixArgString("-", undefined!), ArgStringValueError);
+        });
+
+        test(`null value constructor argument throws ${ArgStringValueError.name}`, function testStringPrefixArgStringConstructor_ThrowsArgStringValueError_WhenPassedNullValueArgument()
+        {
+            assert.throws(() => new StringPrefixArgString("-", null!), ArgStringValueError);
+        });
+
+        test("undefined prefix and value constructor arguments throws", function testStringPrefixArgStringConstructor_Throws_WhenPassedUndefinedPrefixAndValueArguments()
+        {
+            assert.throws(() => new StringPrefixArgString(undefined!, undefined!));
+        });
+
+        test("null prefix and value constructor arguments throws", function testStringPrefixArgStringConstructor_Throws_WhenPassedNullPrefixAndValueArguments()
+        {
+            assert.throws(() => new StringPrefixArgString(null!, null!));
+        });
+
+        test("undefined prefix and null value constructor arguments throws", function testStringPrefixArgStringConstructor_Throws_WhenPassedUndefinedPrefixAndNullValueArguments()
+        {
+            assert.throws(() => new StringPrefixArgString(undefined!, null!));
+        });
+
+        test("null prefix and undefined value constructor arguments throws", function testStringPrefixArgStringConstructor_Throws_WhenPassedNullPrefixAndUndefinedValueArguments()
+        {
+            assert.throws(() => new StringPrefixArgString(null!, undefined!));
+        });
+    });
+});
