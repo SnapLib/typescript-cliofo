32ba80c0620dc3d9cd8c38766192272640821954
Snap <XxSnapperGeexX@gmail.com>
2023-08-22 11:26:55 -0700

    Fixup doc comments in constraint-violation-error module.

--- src/main/ts/lib/constrained-string/constraint-violation-error.ts
+++ src/main/ts/lib/constrained-string/constraint-violation-error.ts
@@ -1,22 +1,32 @@
 /**
- * This module contains the {@link PrefixConstraintViolationError} and
- * {@link ValueConstraintViolationError} error classes thrown by the
- * {@link arg-string-constraint.ArgStringConstraint} and
- * {@link constrained-arg-string.ConstrainedArgString} classes if validation for
- * either a prefix or value fails.
+ * This module contains the {@link PrefixConstraintViolationError} and {@link ValueConstraintViolationError} error
+ * classes thrown by the {@link arg-string-constraint.ArgStringConstraint} and
+ * {@link constrained-arg-string.ConstrainedArgString} classes if validation for either a prefix or value fails.
  *
  * @module constraint-violation-error
+ *
+ * @see {@link arg-string-constraint}
+ * @see {@link constrained-arg-string}
  */
 
 /**
  * Error thrown by {@link arg-string-constraint.ArgStringConstraint.requireValidPrefix ArgStringConstraint.prototype.requireValidPrefix(prefix,string?)}
  * method and the {@link constrained-arg-string.ConstrainedArgString.constructor ConstrainedArgString constructor}
  * if a `prefix` fails validation.
+ *
+ * @see {@link arg-string-constraint.ArgStringConstraint.requireValidPrefix ArgStringConstraint.prototype.requireValidPrefix}
+ * @see {@link constrained-arg-string.ConstrainedArgString.constructor ConstrainedArgString() constructor}
  */
 export class PrefixConstraintViolationError extends Error
 {
+    /** The name for the type of error. */
     public override readonly name: string = PrefixConstraintViolationError.name;
 
+    /**
+     * Constructs a new {@link PrefixConstraintViolationError} with the optional `string` message.
+     *
+     * @param message The error message.
+     */
     public constructor(message?: string)
     {
         super(message);
@@ -26,13 +36,21 @@ export class PrefixConstraintViolationError extends Error
 
 /**
  * Error thrown by {@link arg-string-constraint.ArgStringConstraint.requireValidValue ArgStringConstraint.prototype.requireValidValue(prefix,string,string?)}
- * method and the {@link constrained-arg-string.ConstrainedArgString.constructor ConstrainedArgString constructor}
- * if a `string` value fails validation.
+ * method and the {@link constrained-arg-string.ConstrainedArgString.constructor ConstrainedArgString constructor} if a `string` value fails validation.
+ *
+ * @see {@link arg-string-constraint.ArgStringConstraint.requireValidValue ArgStringConstraint.prototype.requireValidValue}
+ * @see {@link constrained-arg-string.ConstrainedArgString.constructor ConstrainedArgString() constructor}
  */
 export class ValueConstraintViolationError extends Error
 {
+    /** The name for the type of error. */
     public override readonly name: string = ValueConstraintViolationError.name;
 
+    /**
+     * Constructs a new {@link ValueConstraintViolationError} with the optional `string` message.
+     *
+     * @param message The error message.
+     */
     public constructor(message?: string)
     {
         super(message);
