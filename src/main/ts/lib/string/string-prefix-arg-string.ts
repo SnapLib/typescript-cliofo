50cbb33ba88b19978d2cc1628a950b07e2488b2f
Snap <XxSnapperGeexX@gmail.com>
2023-08-22 00:42:50 -0700

    Fixup doc comments in string-prefix-arg-string module.

--- src/main/ts/lib/string/string-prefix-arg-string.ts
+++ src/main/ts/lib/string/string-prefix-arg-string.ts
@@ -1,23 +1,24 @@
 /**
- * This module contains the {@link StringPrefixArgString} class (and its factory
- * methods) used to create objects to represent a string argument that can be
- * passed on the command line consisting of a `string` prefix prepended to a
- * `string` value. The Cliofo API uses this class to represent ***flag*** and
- * ***option*** arguments. For example, consider the following 2 arguments:
+ * This module contains the {@link StringPrefixArgString} class (and its factory methods) used to create objects to
+ * represent a string argument that can be passed on the command line consisting of a `string` prefix prepended to a
+ * `string` value. The Cliofo API uses this class to represent ***flag*** and ***option*** arguments. For example,
+ * consider the following 2 arguments:
  *
  * ```text
  * -h --help
  * ```
  *
- * The `-h` argument could be thought of as a flag consisting of the `'-'`
- * prefix `string` prepended to the `'h'` `string` value.
+ * The `-h` argument could be thought of as a flag consisting of the `'-'` prefix `string` prepended to the `'h'`
+ * `string` value.
  *
- * The `--help` argument could be thought of as an option consisting of the
- * `"--"` prefix `string` prepended to the `"help"` `string` value.
+ * The `--help` argument could be thought of as an option consisting of the `"--"` prefix `string` prepended to the
+ * `"help"` `string` value.
  *
  * @module string-prefix-arg-string
  *
  * @see {@link arg-string}
+ * @see {@link flag-arg-string}
+ * @see {@link option-arg-string}
  */
 
 import { ArgString } from "./arg-string.js";
@@ -26,10 +27,12 @@ import { inspect } from "util";
 const stringToString = (aString: string) => aString.length != 1 ? `"${aString}"` : `'${aString}'`;
 
 /**
- * This class is used to create objects that consists of a `string` {@link ArgString.prefix}
- * prepended to a `string` {@link ArgString.value} to create its {@link ArgString.prefixedValue}.
+ * This class is used to create objects that consists of a `string` {@link ArgString.prefix} prepended to a `string`
+ * {@link ArgString.value} to create its `string` {@link ArgString.prefixedValue}.
  *
  * @see {@link ArgString}
+ * @see {@link flag-arg-string.FlagArgString}
+ * @see {@link option-arg-string.OptionArgString}
  */
 export class StringPrefixArgString extends ArgString<string>
 {
@@ -37,22 +40,18 @@ export class StringPrefixArgString extends ArgString<string>
     readonly #string: string;
 
     /**
-     * Constructs an object with the provided `string` prefix and value. If either
+     * Constructs an object with the provided `string` {@link ArgString.prefix} and {@link ArgString.value}. If either
      * of the arguments are `undefined` or `null`, an error will be thrown.
      *
-     * @param prefix The `string` prepended to the beginning of the constructed
-     *               object's {@link StringPrefixArgString.value} to create its
-     *               {@link StringPrefixArgString.prefixedValue}.
+     * @param prefix The `string` prepended to the beginning of the constructed object's
+     *               {@link StringPrefixArgString.value} to create its {@link StringPrefixArgString.prefixedValue}.
      *
-     * @param value  The `string` appended to the end of the constructed
-     *               object's {@link StringPrefixArgString.prefix} to create its
-     *               {@link StringPrefixArgString.prefixedValue}.
+     * @param value  The `string` appended to the end of the constructed object's {@link StringPrefixArgString.prefix}
+     *               to create its {@link StringPrefixArgString.prefixedValue}.
      *
-     * @throws {@link arg-string.ArgStringPrefixError}
-     * if `undefined` or `null` is passed for the prefix argument.
+     * @throws {@link arg-string.ArgStringPrefixError} if `undefined` or `null` is passed for the prefix argument.
      *
-     * @throws {@link arg-string.ArgStringValueError}
-     * if `undefined` or `null` is passed for the value argument.
+     * @throws {@link arg-string.ArgStringValueError} if `undefined` or `null` is passed for the value argument.
      */
     public constructor(prefix: NonNullable<string>, value: NonNullable<string>)
     {
@@ -62,24 +61,21 @@ export class StringPrefixArgString extends ArgString<string>
     }
 
     /**
-     * This object's prefixed `string` value property consisting of its
-     * {@link StringPrefixArgString.prefix} `string` property prepended to its
-     * {@link StringPrefixArgString.value} `string` property.
+     * This object's prefixed `string` value property consisting of its {@link StringPrefixArgString.prefix} `string`
+     * property prepended to its {@link StringPrefixArgString.value} `string` property.
      */
     public override get prefixedValue(): string { return this.#prefixedValue; }
 
     /**
-     * Returns `true` if the passed argument is a {@link StringPrefixArgString} object
-     * with {@link StringPrefixArgString.prefix} and {@link StringPrefixArgString.value}
-     * `string` properties equal to this object's prefix and value `string` properties.
+     * Returns `true` if the passed argument is a {@link StringPrefixArgString} object with
+     * {@link StringPrefixArgString.prefix} and {@link StringPrefixArgString.value} `string` properties equal to this
+     * object's prefix and value `string` properties.
      *
-     * @param obj The argument being compared for equality to this
-     *            {@link StringPrefixArgString} object.
+     * @param obj The argument being compared for equality to this {@link StringPrefixArgString} object.
      *
-     * @returns `true` if the passed argument is equal to this
-     *          {@link StringPrefixArgString} object.
+     * @returns `true` if the passed argument is equal to this {@link StringPrefixArgString} object.
      */
-    public override equals(obj?: unknown): boolean
+    public override equals(obj: unknown | undefined | null): boolean
     {
         return    this === obj || obj instanceof StringPrefixArgString
                && super.prefix === obj.prefix && super.value === obj.value;
@@ -91,38 +87,32 @@ export class StringPrefixArgString extends ArgString<string>
 }
 
 /**
- * Factory method for creating a {@link StringPrefixArgString} object instance
- * with the specified `string` {@link StringPrefixArgString.prefix} and
- * {@link StringPrefixArgString.value}.
+ * Factory method for creating a {@link StringPrefixArgString} object instance with the specified `string`
+ * {@link StringPrefixArgString.prefix} and {@link StringPrefixArgString.value}.
  *
- * @param prefix The prefix `string` prepended to the beginning of the
- *               returned object's {@link StringPrefixArgString.value} to create
- *               its {@link StringPrefixArgString.prefixedValue} property.
+ * @param prefix The prefix `string` prepended to the beginning of the returned object's
+ *               {@link StringPrefixArgString.value} to create its {@link StringPrefixArgString.prefixedValue} property.
  *
- * @param value  The `string` appended to the end of the returned
- *               {@link StringPrefixArgString}'s {@link StringPrefixArgString.prefix}
- *               to create its {@link StringPrefixArgString.prefixedValue} property.
+ * @param value  The `string` appended to the end of the returned {@link StringPrefixArgString}'s
+ *               {@link StringPrefixArgString.prefix} to create its {@link StringPrefixArgString.prefixedValue} property.
  *
- * @returns A {@link StringPrefixArgString} object with its {@link StringPrefixArgString.prefix}
- *          and {@link StringPrefixArgString.value} properties set to the passed
- *          `string` arguments.
+ * @returns A {@link StringPrefixArgString} object with its {@link StringPrefixArgString.prefix} and
+ *          {@link StringPrefixArgString.value} properties set to the passed `string` arguments.
  *
  * @see {@link StringPrefixArgString}
  */
 export function stringPrefixArgString(prefix: NonNullable<string>, value: NonNullable<string>): StringPrefixArgString;
 
 /**
- * Factory method for creating a {@link StringPrefixArgString} object instance
- * by copying the `string` {@link StringPrefixArgString.prefix} and
- * {@link StringPrefixArgString.value} properties from another
+ * Factory method for creating a {@link StringPrefixArgString} object instance by copying the `string`
+ * {@link StringPrefixArgString.prefix} and {@link StringPrefixArgString.value} properties from another
  * {@link StringPrefixArgString} object.
  *
- * @param other The object to copy properties from to the returned
- *              {@link StringPrefixArgString} object.
+ * @param other The object to copy properties from to the returned {@link StringPrefixArgString} object.
  *
- * @returns A {@link StringPrefixArgString} object with its {@link StringPrefixArgString.prefix}
- *          and {@link StringPrefixArgString.value} properties copied from the
- *          passed {@link StringPrefixArgString} object argument.
+ * @returns A {@link StringPrefixArgString} object with its {@link StringPrefixArgString.prefix} and
+ *          {@link StringPrefixArgString.value} properties copied from the passed {@link StringPrefixArgString} object
+ *          argument.
  *
  * @see {@link StringPrefixArgString}
  */
