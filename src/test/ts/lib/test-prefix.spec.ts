ff21b4fd4a42f351f6881fa5ee090a90e4d982dc
Snap <XxSnapperGeexX@gmail.com>
2023-09-05 18:17:19 -0700

    Remove types from Prefix unit tests.

--- src/test/ts/lib/test-prefix.spec.ts
+++ src/test/ts/lib/test-prefix.spec.ts
@@ -23,11 +23,11 @@ suite(`${Prefix.name} class`, function testSuitePrefixClass()
 
         suite(`${Prefix.name} constructor whitespace arguments`, function testSuitePrefixConstructorWhitespaceArguments()
         {
-            const whitespaceChars: readonly string[] = Object.freeze(["\u0020", "\t", "\n", "\r", "\v"]);
+            const whitespaceChars = Object.freeze(["\u0020", "\t", "\n", "\r", "\v"]);
 
             whitespaceChars.forEach(whiteSpaceChar =>
             {
-                const whitespaceCharName: string =   whiteSpaceChar === "\t" ? "Horizontal tab"
+                const whitespaceCharName =   whiteSpaceChar === "\t" ? "Horizontal tab"
                                                    : whiteSpaceChar === "\n" ? "Line feed"
                                                    : whiteSpaceChar === "\r" ? "Carriage return"
                                                    : whiteSpaceChar === "\v" ? "Vertical tab"
@@ -41,7 +41,7 @@ suite(`${Prefix.name} class`, function testSuitePrefixClass()
 
         test(`String greater than 1 character Prefix constructor argument throws "${PrefixFlagCharError.name}".`, function testPrefixConstructor_ThrowsPrefixFlagCharError_WhenPassedStringGreaterThanSingleCharacter()
         {
-            const aString: string = "***";
+            const aString = "***";
             assert.throws(() => new Prefix(aString), PrefixFlagCharError);
         });
 
@@ -55,38 +55,38 @@ suite(`${Prefix.name} class`, function testSuitePrefixClass()
     {
         test(`${Prefix.name} flag char getter returns a string.`, function testPrefixFlagCharGetter_ReturnsString_WhenCalled()
         {
-            const flagChar: string = "-";
+            const flagChar = "-";
 
-            const prefix: Prefix = new Prefix(flagChar);
+            const prefix = new Prefix(flagChar);
 
             assert.isString(prefix.flagChar, `${prefix.flagChar} is not a string.`);
         });
 
         test(`${Prefix.name} option string getter returns a string.`, function testPrefixOptionStringCharGetter_ReturnsString_WhenCalled()
         {
-            const flagChar: string = "-";
+            const flagChar = "-";
 
-            const prefix: Prefix = new Prefix(flagChar);
+            const prefix = new Prefix(flagChar);
 
             assert.isString(prefix.optionString, `${prefix.optionString} is not a string.`);
         });
 
         test(`${Prefix.name} flag char getter returns flag char.`, function testPrefixFlagCharGetter_ReturnsFlagChar_WhenCalled()
         {
-            const flagChar: string = "-";
+            const flagChar = "-";
 
-            const prefix: Prefix = new Prefix(flagChar);
+            const prefix = new Prefix(flagChar);
 
             assert.strictEqual(prefix.flagChar, flagChar, `'${prefix.flagChar}' does not equal '${flagChar}'.`);
         });
 
         test(`${Prefix.name} option string getter returns flag char repeated twice.`, function testPrefixOptionStringGetter_ReturnsFlagCharRepeatedTwice_WhenCalled()
         {
-            const flagChar: string = "-";
+            const flagChar = "-";
 
-            const prefix: Prefix = new Prefix(flagChar);
+            const prefix = new Prefix(flagChar);
 
-            const expectedOptionString: string = flagChar.repeat(2);
+            const expectedOptionString = flagChar.repeat(2);
 
             assert.strictEqual(prefix.optionString, expectedOptionString, `"${prefix.optionString}" does not equal "${expectedOptionString}".`);
         });
@@ -96,33 +96,33 @@ suite(`${Prefix.name} class`, function testSuitePrefixClass()
     {
         test(`${Prefix.name} equals same returns true.`, function testPrefixEquals_ReturnsTrue_WhenPassedSame()
         {
-            const prefix: Prefix = new Prefix("-");
+            const prefix = new Prefix("-");
             assert.isTrue(prefix.equals(prefix), "Prefix equals same did not return true");
         });
 
         test(`${Prefix.name} equals equivalent returns true.`, function testPrefixEquals_ReturnsTrue_WhenPassedEqual()
         {
-            const aPrefixObject: Prefix = new Prefix("-");
-            const anEqualPrefixObject: Prefix = new Prefix("-");
+            const aPrefixObject = new Prefix("-");
+            const anEqualPrefixObject = new Prefix("-");
             assert.isTrue(aPrefixObject.equals(anEqualPrefixObject), "Prefix equals equivalent did not return true");
         });
 
         test(`${Prefix.name} equals undefined returns false.`, function testPrefixEquals_ReturnsFalse_WhenPassedUndefined()
         {
-            const prefix: Prefix = new Prefix("-");
+            const prefix = new Prefix("-");
             assert.isFalse(prefix.equals(undefined), "Prefix equals undefined did not return false");
         });
 
         test(`${Prefix.name} equals null returns false.`, function testPrefixEquals_ReturnsFalse_WhenPassedNull()
         {
-            const prefix: Prefix = new Prefix("-");
+            const prefix = new Prefix("-");
             assert.isFalse(prefix.equals(null), "Prefix equals null did not return false");
         });
 
         test(`${Prefix.name} equals unequal returns false.`, function testPrefixEquals_ReturnsFalse_WhenPassedUnequal()
         {
-            const aPrefixObject: Prefix = new Prefix("-");
-            const anUnequalPrefixObject: Prefix = new Prefix("+");
+            const aPrefixObject = new Prefix("-");
+            const anUnequalPrefixObject = new Prefix("+");
             assert.isFalse(aPrefixObject.equals(anUnequalPrefixObject), "Prefix equals unequal did not return false");
         });
     });
@@ -151,7 +151,7 @@ suite(`${Prefix.name} factory method`, function testSuitePrefixFactoryMethod()
 
             whitespaceChars.forEach(whiteSpaceChar =>
             {
-                const whitespaceCharName: string =   whiteSpaceChar === "\t" ? "Horizontal tab"
+                const whitespaceCharName =   whiteSpaceChar === "\t" ? "Horizontal tab"
                                                    : whiteSpaceChar === "\n" ? "Line feed"
                                                    : whiteSpaceChar === "\r" ? "Carriage return"
                                                    : whiteSpaceChar === "\v" ? "Vertical tab"
@@ -165,7 +165,7 @@ suite(`${Prefix.name} factory method`, function testSuitePrefixFactoryMethod()
 
         test(`String greater than 1 character ${Prefix.name} factory argument throws "${PrefixFlagCharError.name}".`, function testPrefixFactory_ThrowsPrefixFlagCharError_WhenPassedStringGreaterThanSingleCharacter()
         {
-            const aString: string = "***";
+            const aString = "***";
             assert.throws(() => new Prefix(aString), PrefixFlagCharError);
         });
 
@@ -176,9 +176,9 @@ suite(`${Prefix.name} factory method`, function testSuitePrefixFactoryMethod()
 
         test(`Copy factory method returns equal ${Prefix.name} object.`, function testPrefixFactory_ReturnsEqual_WhenCopying()
         {
-            const original: Prefix = new Prefix("-");
+            const original = new Prefix("-");
 
-            const copy: Prefix = prefix(original);
+            const copy = prefix(original);
 
             assert.isTrue(copy.equals(original));
         });
