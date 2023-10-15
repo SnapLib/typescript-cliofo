90d2ae0ac25ce3b420f9dd23dd48a1db2fcc8c12
Snap <XxSnapperGeexX@gmail.com>
2023-10-14 23:23:54 -0700

    Move mochaewsome config into root mocharc and use env variable to trigger in script.

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,12 +1,31 @@
 "use strict";
 
-module.exports = {
-  "check-leaks": true,
-  "node-option": [
-    "loader=ts-node/esm"
-  ],
-  recursive: true,
-  require: "ts-node/register",
-  spec: "./src/test/ts/**/*.test.mts",
-  ui: "tdd"
-}
+const resolvePath = require("node:path").resolve;
+
+const defaultMochaConfig = {
+    "check-leaks": true,
+    "node-option": [
+        "loader=ts-node/esm"
+    ],
+    package: "./package.json",
+    recursive: true,
+    require: "ts-node/register",
+    spec: "./src/test/ts/**/*.test.mts",
+    ui: "tdd"
+};
+
+const mochawesomeConfig = {
+    reporter: "mochawesome",
+    reporterOptions: [
+        "autoOpen=true",
+        "consoleReporter=none",
+        "json=false",
+        "reportDir=./build/test-report",
+        "reportFilename=index",
+        "reportPageTitle=Cliofo test report",
+        "reportTitle=SnapLib Cliofo Test Report"
+    ]
+};
+
+module.exports = process.env.MOCHA_REPORTER === "mochawesome"
+    ? {...defaultMochaConfig, ...mochawesomeConfig} : defaultMochaConfig;
