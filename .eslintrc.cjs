2fb7637f7fa085fe71ab4b887601dd2eeec0c246
Snap <XxSnapperGeexX@gmail.com>
2023-11-07 23:59:08 -0800

    Add stylistic plugin and start adding more rules.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,6 @@ module.exports = {
         mocha: true,
         node: true
     },
-    extends:
-    [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
     parser: "@typescript-eslint/parser",
     parserOptions:
     {
@@ -18,22 +13,43 @@ module.exports = {
     },
     plugins:
     [
+        "@stylistic",
         "@typescript-eslint"
     ],
-    rules:
-    {
-        "@typescript-eslint/no-inferrable-types": "off",
-        "@typescript-eslint/no-empty-function": "warn",
-        "default-case": "error",
-        "default-case-last": "error",
-        "eol-last": "error",
-        "no-console": "off",
-        "no-empty-function": "off",
-        "no-implicit-coercion": "error",
-        quotes: ["error", "double", {"avoidEscape": true, "allowTemplateLiterals": true}],
-        "no-tabs": "error",
-        "no-trailing-spaces": "error",
-        semi: "off",
-        "@typescript-eslint/semi": ["error", "always"]
-    }
+    overrides:
+    [
+        {
+            files: ["./src/main/ts/**/*.mts"],
+            extends:
+            [
+                "eslint:recommended",
+                "plugin:@typescript-eslint/recommended"
+            ],
+            parserOptions:
+            {
+                project: ["./tsconfig.json"]
+            },
+            rules:
+            {
+                "@typescript-eslint/adjacent-overload-signatures": "error",
+                "@typescript-eslint/array-type": ["error", { "default": "array-simple" }],
+                "@typescript-eslint/await-thenable": "error",
+                "@typescript-eslint/no-inferrable-types": "off",
+                "@typescript-eslint/no-empty-function": "warn",
+                "@stylistic/block-spacing": "error",
+                "@stylistic/brace-style": ["error", "allman", { "allowSingleLine": true }],
+                "default-case": "error",
+                "default-case-last": "error",
+                "eol-last": "error",
+                "no-console": "off",
+                "no-empty-function": "off",
+                "no-implicit-coercion": "error",
+                quotes: ["error", "double", {"avoidEscape": true, "allowTemplateLiterals": true}],
+                "no-tabs": "error",
+                "no-trailing-spaces": "error",
+                semi: "off",
+                "@typescript-eslint/semi": ["error", "always"]
+            }
+        }
+    ],
 };
